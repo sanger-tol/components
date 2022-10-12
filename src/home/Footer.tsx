77c7fedd7c0011f1221aa490c1730c0b5ec75111
Kiernan Harding <kh16@sanger.ac.uk>
2022-10-12 14:24:18 +0100

    TOLP-4521: Create a React library skeleton

---
+++ src/home/Footer.tsx
@@ -0,0 +1,27 @@
+/*
+SPDX-FileCopyrightText: 2022 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+*/
+
+import React from "react";
+import { Container } from "react-bootstrap"
+
+
+function Footer() {
+  return (
+    <div className="footer">
+      <footer className="py-2 bg-black fixed-bottom">
+        <Container>
+          <p className="m-0 text-center text-white small">Maintained by
+            <a href="https://sanger.ac.uk"> Wellcome Sanger Institute</a>
+            , <a href="https://www.sanger.ac.uk/programme/tree-of-life/">Tree of Life Programme</a>
+            , <a href="mailto:tol-platforms@sanger.ac.uk">Enabling Platforms Team</a>
+          </p>
+        </Container>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
