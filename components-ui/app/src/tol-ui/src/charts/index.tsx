74a8a32a3f3639348dd081612b974a62ed07bb0a
Kiernan Harding <kh16@sanger.ac.uk>
2023-07-05 13:40:24 +0100

    import

---
+++ components-ui/app/src/tol-ui/src/charts/index.tsx
@@ -0,0 +1,10 @@
+/*
+SPDX-FileCopyrightText: 2022 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+*/
+
+export { default as BarChart } from "./BarChart";
+export { default as RemoteBarChart } from "./RemoteBarChart";
+export { default as RemoteChartTable } from "./RemoteChartTable";
+export { default as RemoteDateChartTable } from "./RemoteDateChartTable";
