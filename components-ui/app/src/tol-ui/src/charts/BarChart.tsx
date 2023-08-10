0181bd46194246a6425bfa21d00530780b4c5999
ene goktan <goktan.ene@gmail.com>
2023-08-10 16:38:09 +0100

    change highlighted data on different legend click

--- components-ui/app/src/tol-ui/src/charts/BarChart.tsx
+++ components-ui/app/src/tol-ui/src/charts/BarChart.tsx
@@ -93,43 +93,22 @@ function BarChart(props: Props) {
         }
       },
       legend: {
-        // onClick: (event: any, legendItem: any, legend: any) => {
-        //   if (isPropDefined(setBarData)) {
-        //     // find the index of the selected dataset
-        //     const ci = legend.chart
-        //     const index = ci.data.datasets.findIndex(obj => obj.label === legendItem.text)
-        //     console.log(legendItem)
-
-        //     // if index is clicked on
-        //     if (ci.isDatasetVisible(index)) {
-        //       ci.hide(index)
-        //       legendItem.hidden = true
-        //     } else {
-        //       ci.show(index)
-        //       legendItem.hidden = false
-        //     }
-
-        //   }
-        // },
-
         onClick: (event: any, legendItem: any, legend: any) => {
-          console.log(legend)
-          debugger
           const legendIndex = legend.chart.data.datasets.findIndex(obj => obj.label === legendItem.text)
 
           legend.chart.data.datasets.forEach((dataset, index) => {
             if (index === legendIndex) {
-              console.log('no change to ', dataset);
+              dataset.backgroundColor = dataset.backgroundColor.map((color) => {
+                // regex to replace the alpha channel value - put into a function later
+                return color.replace(/[\d.]+\)$/g, '1)')
+              })
             } else {
-              // dataset.backgroundColor.forEach((color, colorIndex, colors) => {
-              //   color = color.replace(/[\d.]+\)$/g, '0.15)')
               dataset.backgroundColor = dataset.backgroundColor.map((color) => {
                 return color.replace(/[\d.]+\)$/g, '0.20)')
               })
             }
           })
           legend.chart.update()
-          console.log(legend.chart.data)
         },
 
         labels: {
