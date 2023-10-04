4ef459c25941cb5b3d7025c9e8f1bc679d6e171d
Edward Moulsdale <em29@sanger.ac.uk>
2023-10-04 10:07:24 +0100

    TOLP-6468 Standardise (non-plus) URL encoding in Components

--- components-ui/app/src/tol-ui/src/services/http/config.ts
+++ components-ui/app/src/tol-ui/src/services/http/config.ts
@@ -4,11 +4,26 @@
 
 import { env } from '../../variables/config'
 
+const serializeParam = (k, v) => {
+  const sK = encodeURIComponent(k);
+  const sV = encodeURIComponent(
+    JSON.stringify(v)
+  );
+  return `${sK}=${sV}`
+}
+
+const serializeParams = params => Object.entries(params).filter(
+  ([_, v]) => v !== undefined
+).map(
+  ([k, v]) => serializeParam(k, v)
+).join('&');
+
 export const CONFIG = {
   baseURL: env.API_PATH,
   headers: {
     'Content-Type': 'application/json',
   },
+  paramsSerializer: serializeParams,
 };
 
 export const END_POINT = {
