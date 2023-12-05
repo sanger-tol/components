da40d8be5b2f1309c9e631a0e51e1d731698857d
ene goktan <eg21@sanger.ac.uk>
2023-12-05 15:48:41 +0000

    format date and normalise caps

--- components-ui/app/src/tol-ui/src/general/ObjectDetail.tsx
+++ components-ui/app/src/tol-ui/src/general/ObjectDetail.tsx
@@ -4,24 +4,35 @@ SPDX-FileCopyrightText: 2023 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
+import { formatDate, normaliseCaps } from './Utils'
+
+
+const updateContents = (contents: object) => {
+    for (const [key, value] of Object.entries(contents)) {
+      // remove or format some content
+      if (typeof value === 'string' && value.includes('GMT')) {
+        contents[key] = formatDate(value)
+      }
+    }
+    return contents
+  }
 
 export interface Props {
     data: object
+    // fields?: FieldMetaData
 }
 
 const ObjectDetail = (props: Props) => {
     const { data } = props
-
     return (
         <div className='tol-object-detail'>
             <h2 className='mb-2'>Info</h2>
-            {Object.entries(data).map(([key, value]) =>
+            {Object.entries(updateContents(data)).map(([key, value]) =>
             <p key={key}>
-                <strong>{key}:</strong> {value}
+                <strong>{normaliseCaps(key)}:</strong> {value}
             </p>)}
         </div>
         )
 }
-
 
 export default ObjectDetail;
