042b1d84b7f25996581944f8d4acf794c78a7548
Ene Goktan <eg21@sanger.ac.uk>
2024-01-02 13:09:05 +0000

    TOLP-6823 & 6824 Remote object detail component

---
+++ components-ui/app/src/tol-ui/src/general/ObjectDetail.tsx
@@ -0,0 +1,23 @@
+/*
+SPDX-FileCopyrightText: 2023 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+*/
+
+export interface Props {
+  data: object
+}
+
+const ObjectDetail = (props: Props) => {
+  const { data } = props
+  return (
+    <div className='tol-object-detail'>
+      {Object.entries(data).map(([key, value]) =>
+        <p key={key}>
+          <strong>{key}:</strong> {value}
+        </p>)}
+    </div>
+  )
+}
+
+export default ObjectDetail;
---
+++ components-ui/app/src/tol-ui/src/general/RemoteObjectDetail.tsx
@@ -0,0 +1,88 @@
+/*
+SPDX-FileCopyrightText: 2023 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+*/
+
+import { useState, useEffect } from "react";
+import { httpClient } from '../services/http/httpClient'
+import ObjectDetail from "./ObjectDetail";
+import { FieldMetaData } from "../table/FieldMeta";
+import { formatDate, normaliseCaps } from "./Utils";
+
+interface Props {
+    endpoint: string,
+    fields: FieldMetaData
+    filter?: object,
+    baseUrl?: string,
+}
+
+const formatContents = (contents: object) => {
+    const updatedContents: any = {}
+    for (const [key, value] of Object.entries(contents)) {
+        // format keys
+        const formattedKey = normaliseCaps(key)
+
+        // format values if date or boolean
+        let formattedValue = value
+        if (typeof value === 'string' && value.includes('GMT')) {
+            formattedValue = formatDate(value)
+        }
+        if (typeof value === 'boolean') {
+            formattedValue = value.toString()
+        }
+    
+        updatedContents[formattedKey] = formattedValue
+    }
+    return updatedContents
+  }
+
+const RemoteObjectDetail = (props: Props) => {
+    const { endpoint, filter, baseUrl, fields } = props
+    const [ objectData, setObjectData ] = useState<any[]>([]);
+
+    useEffect(() => {
+        httpClient().get('/' + endpoint, {
+            baseURL: baseUrl,
+            params: {
+                filter: filter
+            }
+        })
+        .then((res: any) => {
+            const data = res.data.data[0]
+            let selectedFields: any = {}
+
+            if (data !== undefined) {
+                Object.entries(fields).forEach(([fieldKey, fieldValues]) => {
+                    if (fieldKey in data.attributes) {
+                        let value = data.attributes[fieldKey]
+                        
+                        // rename the field if rename value provided
+                        if (fieldValues.rename !== undefined && fieldValues.rename !== null) {
+                            selectedFields[fieldValues.rename] = value
+                        } else {
+                            selectedFields[fieldKey] = value
+                        }
+                    } else {
+                        console.warn(`Field '${fieldKey}' is missing in the data attributes.`)
+                    }
+                })
+                selectedFields = formatContents(selectedFields)
+                setObjectData(selectedFields)
+            }
+        }).catch((error: any) => {
+            console.warn(error.message)
+            console.warn('Please ensure the db has been restored')
+            console.warn('Please ensure the \'endpoint\' prop is correct and pluralised')
+            console.warn('Please ensure the \'fields\' prop is provided')
+        })
+    }, [endpoint, filter, baseUrl])
+
+    return (
+        <ObjectDetail 
+        data={ objectData }
+        />
+    )
+}
+
+export default RemoteObjectDetail;
