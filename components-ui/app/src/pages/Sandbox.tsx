00622b6781cd0f2321a05ceaf49551d09d83c789
Logan Howat <lh31@sanger.ac.uk>
2024-02-14 11:44:43 +0000

    attempting to get things rendered without using widgets component

--- components-ui/app/src/pages/Sandbox.tsx
+++ components-ui/app/src/pages/Sandbox.tsx
@@ -4,12 +4,23 @@ SPDX-FileCopyrightText: 2023 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
+import { useState } from 'react';
+
 import {
-  ResponsiveWidget
+  ResponsiveWidget,
+  RemoteBarChart,
+  RemoteTable,
+  env
 } from '../tol-ui/src';
 
 
 function Sandbox() {
+
+  const [combinedFilters, setCombinedFilters] = useState<object>({});
+  const [tableFilter, setTableFilter] = useState<object>({});
+  const [globalFilters, setGlobalFilters] = useState<object>({
+    in_list: {}
+  });
 
   const title = (
     <span>
@@ -17,8 +28,79 @@ function Sandbox() {
     </span>
   );
 
+  const chart = (
+    <RemoteBarChart
+      stacked
+      endpoint="barcoding_run_data"
+      breakDownBy="bioscan_o_primary"
+      xAxis="sts_sample.sts_col_date"
+      interval="M"
+      filter={globalFilters}
+      setCombinedFilters={setCombinedFilters}
+      type='date'
+      height={500}
+      baseUrl={ env.TOL_DATA }
+      shortDate
+    />
+  );
+
+  const table = (
+    <RemoteTable
+      id="report-card-v3"
+      endpoint="barcoding_run_data"
+      filter={tableFilter}
+      defaultSort="sts_sample.sts_col_date"
+      height={500}
+      baseUrl={env.TOL_DATA}
+      fields={{
+        "sts_sample.sts_gal_name": {
+          rename: "Partner"
+        },
+        "bioscan_run_primary": {
+          rename: "Batch"
+        },
+        "bioscan_specimen.id": {
+          rename: "Specimen ID"
+        },
+        "bioscan_read_count": {
+          rename: "Reads"
+        },
+        "bioscan_reads_in_contigs": {
+          rename: "Read in Contigs"
+        },
+        "bioscan_contigs_produced": {
+          rename: "Contigs"
+        },
+        "bioscan_rep_count_primary": {
+          rename: "Reads in BIN"
+        },
+        "bioscan_id_similarity_primary": {
+          rename: "% Match to BIN"
+        },
+        "bioscan_pred_tax": {
+          rename: "Predicted Taxonomy"
+        }
+      }}
+    />
+  );
+
+  const components = [
+    {
+     'component': title,
+     'type': 'bar'
+    },
+    {
+     'component': chart,
+     'type': 'bar'
+    },
+    {
+     'component': table,
+     'type': 'table'
+    },
+  ]
+
   return (
-    <ResponsiveWidget components={[title]}/>
+    <ResponsiveWidget components={components}/>
   );
 }
 
--- components-ui/app/src/tol-ui/src/general/ResponsiveWidget.tsx
+++ components-ui/app/src/tol-ui/src/general/ResponsiveWidget.tsx
@@ -4,38 +4,73 @@ SPDX-FileCopyrightText: 2023 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
+import { useState } from 'react';
 import Widgets from "./Widgets";
 import  GridLayout,{ Layout } from 'react-grid-layout';
 
+interface Components {
+  component: JSX.Element,
+  type: string
+}
 
 interface Props {
   title?: string,
   description?: string,
-  components: JSX.Element[],
+  components: Components[],
   items?: number
   cols?: 4,
-  rowheight?: 30
+  rowheight?: 100
 }
+// Could potentially pass components as a key with bar, sunburst, table, filters and count as potential keys that define the size
+
+//function CalculateXPosForWidget(i, prevPosition, setPrev){ //Could use prev x position + sizt to determine where then next component should start from
+//  
+//}
 
 function ResponsiveWidget(props:Props){
-    const { title, description, components, items, cols, rowheight } = props;
-    //const [layout, setLayout] = useState({});
+  const { title, description, components, items, cols, rowheight } = props;
+  const [prevX, setPrevX] = useState({});
+  const [prevY, setPrevY] = useState({});
 
-    // Define layouts for different breakpoints
-    const layout: Layout[] = [
-      { i: 'item1', x: 0, y: 0, w: 1, h: 0 },
-    ];
+  const componentSizeKey = { // Default sizes for the various component types
+    'bar-width': 4,
+    'bar-height': 3,
+    'sunburst-width': 4,
+    'sunburst-height': 4,
+    'table-width': 4,
+    'table-height': 4
+  }
+
+  components.map((component) => {
+    console.log(componentSizeKey[`${component.type}-width`])
+    console.log(componentSizeKey[`${component.type}-height`])
+  })
+
+  // Define layouts for different breakpoints
+  const layouts: Layout[] = components.map((component, index) => ({
+    i: `item${index + 1}`,
+    x: (index % 4) * 2,//CalculateXPosForWidget(index, prevX, setPrevX), // Adjust x position based on index
+    y: Math.floor(index / 4) * 2, // Adjust y position based on index
+    w: componentSizeKey[`${component.type}-width`], // Width of each component
+    h: componentSizeKey[`${component.type}-height`], // Height of each component
+  }));
+
 
   return (
     <GridLayout
       className="layout"
-      layout={layout}
+      layout={layouts}
       cols={4}
-      rowHeight={100}
+      rowHeight={200}
       width={1200}
     >
-      {/* Grid items go here */}
-      <div key="item1"><Widgets components={[components[0]]}/></div>
+      {components.map((component, index)=> {
+        console.log(component.component)
+        const componentToRender = component.component
+        return (
+          <div key={`item${index+1}`}><componentToRender/></div>
+        )
+      })}
     </GridLayout>
   );
 };
