f8abc3028ac9589047381f660fe0817551714dc6
Kiernan Harding <kh16@sanger.ac.uk>
2024-08-05 13:28:48 +0100

    page

--- components-ui/app/src/pages/Sandbox.tsx
+++ components-ui/app/src/pages/Sandbox.tsx
@@ -6,17 +6,86 @@
 
 
 import {
-  Dashboard,
+  Filter,
+  RemoteMap,
+  RemoteSunburst,
+  Widgets,
+  env,
+  useZone,
+  useTranslator
 } from '../tol-ui/src';
 
 
 function Sandbox() {
+  const speciesZone = useZone({
+    endpoint: 'species',
+    baseUrl: env.TOL_DATA,
+    components: [
+      {id: 'filter-1'},
+      {id: 'sunburst-1'}
+    ]
+  });
 
-  return (
+  const sampleZone = useZone({
+    endpoint: 'sample',
+    baseUrl: env.TOL_DATA,
+    components: [
+      {id: 'map-1'}
+    ]
+  });
+
+  useTranslator({
+    source: speciesZone,
+    target: sampleZone,
+    translations: {
+      goat_family_name: 'sts_species.sts_family_name'
+    }
+  })
+
+  const speciesComponent = (
     <div>
-      <Dashboard id='sandbox-zone'/>
+      <Filter
+        attribute='goat_family_name'
+        rename='Family Name'
+        type='str'
+        componentId='filter-1'
+        {...speciesZone}
+      />
+      <div style={{height: 10}}/>
+      <RemoteSunburst
+        title="Example Sunburst"
+        id="sunburst-1"
+        sliceBy={[
+          "goat_family_name",
+          "goat_genus_name"
+        ]}
+        height={400}
+        {...speciesZone}
+      />
     </div>
   );
+
+  const sampleComponent = (
+    <RemoteMap
+      id='map-1'
+      longitudeKey="sts_sample.sts_longitude.keyword"
+      latitudeKey="sts_sample.sts_latitude.keyword"
+      {...sampleZone}
+    />
+  );
+
+  const components = [
+    {
+      component: speciesComponent,
+      type: 'full'
+    },
+    {
+      component: sampleComponent,
+      type: 'lg'
+    }
+  ]
+
+  return <Widgets components={components} />;
 }
 
 export default Sandbox;
