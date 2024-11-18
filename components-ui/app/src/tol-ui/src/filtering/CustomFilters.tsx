2d586097b8d3738c61fdc7a2e268eb33de524378
Logan Howat <lh31@sanger.ac.uk>
2024-11-18 10:19:36 +0000

    Starting custom filters

---
+++ components-ui/app/src/tol-ui/src/filtering/CustomFilters.tsx
@@ -0,0 +1,21 @@
+/*
+SPDX-FileCopyrightText: 2024 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+*/
+
+import { Filter } from "./Filter";
+
+
+interface Props {
+  objectType: string,
+  zone: any,
+  setZone: any,
+  endpoint: string,
+}
+
+function CustomFilters () {
+
+}
+
+export default CustomFilters;
