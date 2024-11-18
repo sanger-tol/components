dada541b7a888aa564df16947a0a7533a55b09b1
Edward Moulsdale <em29@sanger.ac.uk>
2024-11-18 14:28:40 +0000

    TOLP-7711 Database schema to save dashboards and alembic migration

--- components-api/app/main/__init__.py
+++ components-api/app/main/__init__.py
@@ -14,8 +14,9 @@ from tol.api_base2 import (
 )
 from tol.api_base2.misc import default_ctx_getter
 from tol.core import core_data_object
-from tol.sql import create_sql_datasource
+from tol.sql import Model, create_sql_datasource
 from tol.sql.auth import db_auth_blueprint
+from tol.sql.board import create_board_models
 
 from .model import Base, Sample, Singular, Species, Specimen
 
@@ -39,12 +40,44 @@ def __user_id_blueprint(api_path: str) -> None:
     return user_bp
 
 
+def __get_board_models(
+    base_model: type[Model],
+) -> tuple[list[type[Model]], type[Model]]:
+
+    board_models = create_board_models(base_model)
+
+    return list(board_models), board_models._user_mixin
+
+
 def application():
     app = Flask(__name__)
 
+    # the user-configurable dashboards
+    board_models, user_mixin_class = __get_board_models(Base)
+
+    # authentication and authorisation
+    auth_bp = db_auth_blueprint(
+        Base,
+        os.environ['DB_URI'],
+        url_prefix=os.getenv('API_PATH') + '/auth',
+        oidc_id_target='id',
+        user_mixin_class=user_mixin_class
+    )
+    app.register_blueprint(auth_bp)
+    auth_bp.register_authenticator(app)
+
+    models = [
+        Species,
+        Specimen,
+        Sample,
+        Singular,
+        auth_bp.models.user_class,
+        *board_models
+    ]
+
     # Set up datasource
     sql_datasource = create_sql_datasource(
-        models=[Species, Specimen, Sample, Singular],
+        models=models,
         db_uri=os.getenv('DB_URI')
     )
     core_data_object(sql_datasource)
@@ -55,17 +88,8 @@ def application():
                            url_prefix=os.getenv('API_PATH'))
 
     # The system endpoints
-    blueprint_system = system_blueprint(sql_datasource)
+    blueprint_system = system_blueprint()
     app.register_blueprint(blueprint_system, url_prefix=os.getenv('API_PATH') + '/system')
-
-    auth_bp = db_auth_blueprint(
-        Base,
-        os.environ['DB_URI'],
-        url_prefix=os.getenv('API_PATH') + '/auth',
-        oidc_id_target='id'
-    )
-    app.register_blueprint(auth_bp)
-    auth_bp.register_authenticator(app)
 
     user_id_bp = __user_id_blueprint(os.environ['API_PATH'])
     app.register_blueprint(user_id_bp)
---
+++ components-api/migrations/alembic/versions/49115544a7be_add_user_configurable_dashboards.py
@@ -0,0 +1,113 @@
+"""add user-configurable dashboards
+
+Revision ID: 49115544a7be
+Revises: 18927584baf1
+Create Date: 2024-11-18 10:20:09.795475
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import JSONB
+
+
+# revision identifiers, used by Alembic.
+revision = '49115544a7be'
+down_revision = '18927584baf1'
+branch_labels = None
+depends_on = None
+
+
+
+def upgrade():
+    # Create table `component`
+    op.create_table(
+        'component',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('title', sa.String, nullable=False),
+        sa.Column('object_type', sa.String, nullable=False),
+        sa.Column('base_url', sa.String, nullable=False),
+        sa.Column('component_type', sa.String, nullable=False),
+        sa.Column('widget_type', sa.String, nullable=False),
+        sa.Column('config', JSONB, nullable=False),
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('user.id'), nullable=False)
+    )
+
+    # Create table `zone`
+    op.create_table(
+        'zone',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('title', sa.String, nullable=False),
+        sa.Column('object_type', sa.String, nullable=False),
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('user.id'), nullable=False)
+    )
+
+    # Create table `view`
+    op.create_table(
+        'view',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('title', sa.String, nullable=False),
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('user.id'), nullable=False)
+    )
+
+    # Create table `board`
+    op.create_table(
+        'board',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('title', sa.String, nullable=False),
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('user.id'), nullable=False)
+    )
+
+    # Create table `component_zone`
+    op.create_table(
+        'component_zone',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('order', sa.Integer, nullable=False),
+        sa.Column('component_id', sa.Integer, sa.ForeignKey('component.id'), nullable=False),
+        sa.Column('zone_id', sa.Integer, sa.ForeignKey('zone.id'), nullable=False)
+    )
+
+    # Create table `zone_view`
+    op.create_table(
+        'zone_view',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('order', sa.Integer, nullable=False),
+        sa.Column('zone_id', sa.Integer, sa.ForeignKey('zone.id'), nullable=False),
+        sa.Column('view_id', sa.Integer, sa.ForeignKey('view.id'), nullable=False)
+    )
+
+    # Create table `view_board`
+    op.create_table(
+        'view_board',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('order', sa.Integer, nullable=False),
+        sa.Column('view_id', sa.Integer, sa.ForeignKey('view.id'), nullable=False),
+        sa.Column('board_id', sa.Integer, sa.ForeignKey('board.id'), nullable=False)
+    )
+
+    # add order uniqueness constraints
+    op.create_unique_constraint(
+        None,
+        'component_zone',
+        ['zone_id', 'order']
+    )
+    op.create_unique_constraint(
+        None,
+        'zone_view',
+        ['view_id', 'order']
+    )
+    op.create_unique_constraint(
+        None,
+        'view_board',
+        ['board_id', 'order']
+    )
+
+
+def downgrade():
+    # Drop tables in reverse order of creation
+    op.drop_table('board')
+    op.drop_table('view_board')
+    op.drop_table('view')
+    op.drop_table('zone_view')
+    op.drop_table('zone')
+    op.drop_table('component_zone')
+    op.drop_table('component')
