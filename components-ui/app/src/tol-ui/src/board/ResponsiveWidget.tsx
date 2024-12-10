9267fb16b219167b5b17834df5627f6c9fa70c7e
Luke Wilson <lw23@sanger.ac.uk>
2024-12-10 14:44:50 +0000

    try ignoring it instead

--- components-ui/app/src/tol-ui/src/board/ResponsiveWidget.tsx
+++ components-ui/app/src/tol-ui/src/board/ResponsiveWidget.tsx
@@ -43,7 +43,6 @@ function ResponsiveWidget(props: Props) {
   const [layoutsState, setLayouts] = useState<Layouts>(generateLayout(widgets));
   const [confirmationModalOpen, setConfirmationModalOpen] = useState(false);
   const internalLayouts = useRef(generateLayout(widgets));
-  const ref = useRef<any>(null);
 
   useEffect(() => {
     const newLayout = generateLayout(widgets);
@@ -139,6 +138,7 @@ function ResponsiveWidget(props: Props) {
 
   return (
     <div className='tol-responsive-grid'>
+      {/*@ts-ignore*/}
       <ResponsiveReactGridLayout
         layouts={layoutsState}
         breakpoints={{ lg: 992, md: 576, sm: 0 }}
@@ -149,7 +149,6 @@ function ResponsiveWidget(props: Props) {
         onLayoutChange={onLayoutChange}
         onBreakpointChange={onBreakpointChange}
         draggableCancel='.widget-delete-btn'
-        refs={ref}
       >
         {widgets.order.map((key)=> {
           // Check if there is a component that matches the ids
