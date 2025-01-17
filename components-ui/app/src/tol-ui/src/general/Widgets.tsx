a604c65e9b95b044b428292b3b1330cb3a327164
Kiernan Harding <kh16@sanger.ac.uk>
2025-01-17 08:35:19 +0000

    go!

--- components-ui/app/src/tol-ui/src/general/Widgets.tsx
+++ components-ui/app/src/tol-ui/src/general/Widgets.tsx
@@ -24,7 +24,7 @@ function Widgets(props: Props) {
   themeListener(() => {
     try {
       const backing = document.getElementById("tol-app-background");
-      backing!.style.backgroundColor = getCssVarValue("--bs-body-darker");
+      backing!.style.backgroundColor = getCssVarValue("--tol-bg-dark");
     } catch {
       return;
     }
