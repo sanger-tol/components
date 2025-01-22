658006e1869eb3bb57ca3fb73f94d8f2f98f2881
Luke Wilson <lw23@sanger.ac.uk>
2025-01-22 13:54:51 +0000

    fixes

--- components-ui/app/src/tol-ui/src/board/components/MyBoardsHeader.tsx
+++ components-ui/app/src/tol-ui/src/board/components/MyBoardsHeader.tsx
@@ -54,8 +54,8 @@ function MyBoardsHeader(props: Props) {
     } finally {
       if (modalError === "") {
         setTimeout(() => {
-        history.push(`/board/${boardId}`);
-        }, 400)
+          history.push(`/board/${boardId}`);
+        }, 400);
       }
     }
   };
@@ -78,8 +78,8 @@ function MyBoardsHeader(props: Props) {
   );
 
   const defaultDropdownMainIcon = {
-    icon: 'plus',
-    type: 'success'
+    icon: "plus",
+    type: "success",
   };
 
   const {
@@ -95,25 +95,21 @@ function MyBoardsHeader(props: Props) {
   } = props;
 
   return (
-    <div style={containerStyle} className={customClass}>
-      <div>
-        <h1>{title}</h1>
-        <p>{subTitle}</p>
-        <p style={{color:'red'}}>
-          Warning: Dashboards are still in development so existing 
-          boards or views may be removed at any point
-        </p>
+      <div style={containerStyle} className={customClass}>
+        <div>
+          <h1>{title}</h1>
+          <p>{subTitle}</p>
+        </div>
+        <DropdownButtons
+          mainButtonIcon={dropdownMainIcon}
+          placement={placement}
+          globalDisabled={globalDisabled}
+          dropdownButtons={dropdownButtons}
+          menuStyle={menuStyle}
+          showMessages={false}
+        />
+        {newBoardModalOpen && newBoardModal()}
       </div>
-      <DropdownButtons
-        mainButtonIcon={dropdownMainIcon}
-        placement={placement}
-        globalDisabled={globalDisabled}
-        dropdownButtons={dropdownButtons}
-        menuStyle={menuStyle}
-        showMessages={false}
-      />
-      {newBoardModalOpen && newBoardModal()}
-    </div>
   );
 }
 
