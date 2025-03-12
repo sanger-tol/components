3c43c61e0272a9e073b119c0d834640ea1180c53
Logan Howat <lh31@sanger.ac.uk>
2025-03-12 13:24:16 +0000

    Fixing build issues

--- components-ui/app/src/tol-ui/src/general/FormatTooltip.tsx
+++ components-ui/app/src/tol-ui/src/general/FormatTooltip.tsx
@@ -4,11 +4,7 @@ SPDX-FileCopyrightText: 2023 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-<<<<<<< HEAD
-import { formatDate, normaliseCaps } from "./utils";
-=======
-import { formatDate } from "./Utils";
->>>>>>> 586fa264 (Formatting and removing unneeded imports)
+import { formatDate } from "./utils";
 
 const updateContents = (contents: object) => {
   for (const [key, value] of Object.entries(contents)) {
