278680d0f08d021fc86090b5ffc17b886a39c0f1
Luke Wilson <lw23@sanger.ac.uk>
2025-03-13 10:47:15 +0000

    remove sandbox code

--- components-ui/app/src/pages/Sandbox.tsx
+++ components-ui/app/src/pages/Sandbox.tsx
@@ -4,36 +4,8 @@ SPDX-FileCopyrightText: 2023 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-import { RemoteTable, Widgets, useZone } from "../tol-ui/src";
-
 function Sandbox() {
-  const sample = useZone({
-    endpoint: "sample",
-    // baseUrl: env.TOL_DATA,
-    components: [{ id: "table-example-2" }],
-  });
-
-  const table2 = (
-    <div>
-      <h5 style={{ marginBottom: 12 }}>Remote Table Empty On Load</h5>
-      <RemoteTable
-        id="table-example-2"
-        height={500}
-        {...sample}
-        actions={["Export With No Requirements", "Export With Requirements"]}
-        rowSelection
-      />
-    </div>
-  );
-
-  const components = [
-    {
-      component: table2,
-      type: "full",
-    },
-  ];
-
-  return <Widgets components={components} />;
+  return <></>;
 }
 
 export default Sandbox;
