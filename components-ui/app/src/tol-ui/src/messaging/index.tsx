6148003eab6725cf283332aa74840f9a0b1bee01
Kiernan Harding <kh16@sanger.ac.uk>
2025-03-14 12:10:25 +0000

    TOLP-8527: Expose action status on an object_type

--- components-ui/app/src/tol-ui/src/messaging/index.tsx
+++ components-ui/app/src/tol-ui/src/messaging/index.tsx
@@ -4,8 +4,8 @@ SPDX-FileCopyrightText: 2024 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-export { default as Message } from "../messaging/Message";
-export { default as Notification } from "../messaging/Notification";
+export { default as Message } from "./Message";
+export { default as Notification } from "./Notification";
 export { default as StaticMessage } from "./StaticMessage";
 export { default as StatusMessage } from "./StatusMessage";
-export { default as PopUpMessage } from "../messaging/PopUpMessage";
+export { default as PopUpMessage } from "./PopUpMessage";
