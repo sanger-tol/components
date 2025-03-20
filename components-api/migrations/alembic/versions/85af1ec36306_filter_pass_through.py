10e307eddd7984aeafebc8d9cc11df12c42476ac
Logan Howat <lh31@sanger.ac.uk>
2025-03-20 16:11:44 +0000

    Adding alembic migration to add filter_pass_through col

---
+++ components-api/migrations/alembic/versions/85af1ec36306_filter_pass_through.py
@@ -0,0 +1,24 @@
+"""filter_pass_through
+
+Revision ID: 85af1ec36306
+Revises: 7b513d308beb
+Create Date: 2025-03-20 16:05:06.363267
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '85af1ec36306'
+down_revision = '7b513d308beb'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('component', sa.Column('filter_pass_through', sa.Boolean(), nullable=True))
+
+
+def downgrade() -> None:
+    pass
