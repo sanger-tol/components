8c2c71bcbda584db322b8351ecff99b1855a5e0f
Edward Moulsdale <em29@sanger.ac.uk>
2025-04-01 08:43:58 +0100

    refactor again slightly

--- test/playwright/tests/pages.spec.ts
+++ test/playwright/tests/pages.spec.ts
@@ -7,6 +7,9 @@ import { expect } from '@playwright/test';
 import sql from '../db';
 import { test } from '../fixtures';
 
+const userID = "1000021";
+const orcidID = "https://orcid.org/0000-0000-0000-0000";
+
 const headless = !!(process.env.CI || process.env.HEADLESS);
 
 test.use({headless: headless});
@@ -14,11 +17,11 @@ test.use({headless: headless});
 const setAuth = async ({page, token}) => {
   const storageData = {
     user: {
-      "oidc_id": "https://orcid.org/0000-0000-0000-0000",
-      "token_created_at":"2025-03-31T14:13:36.345558",
-      "token_expires_at":"2090-04-07T14:13:36.345581",
-      "id":"1000021",
-      "roles":[]
+      "oidc_id": orcidID,
+      "token_created_at": "2025-03-31T14:13:36.345558",
+      "token_expires_at": "2090-04-07T14:13:36.345581",
+      "id": userID,
+      "roles": []
     },
     'token': token,
   };
@@ -50,13 +53,13 @@ test.beforeAll(async ({token}) => {
   // insert the rest
   try {
     await sql.unsafe(`INSERT INTO "user"
-    VALUES (1000021, 'https://orcid.org/0000-0000-0000-0000');
+    VALUES (${userID}, '${orcidID}');
     
     INSERT INTO role_binding
-    VALUES (83489247, 1000021, 1);
+    VALUES (83489247, ${userID}, 1);
     
     INSERT INTO "token"
-    VALUES (3498237, '${token}', NOW(), NOW() + INTERVAL '1 YEAR', 1000021);`).simple();    
+    VALUES (3498237, '${token}', NOW(), NOW() + INTERVAL '1 YEAR', ${userID});`).simple();    
   }
   catch (e) {
     console.error(e);
@@ -82,12 +85,18 @@ const createBoard = async ({page, testID}) => {
   expect(page.locator('body')).toContainText(testID);
 };
 
+const deleteBoard = async({page, testID}) => {
+
+};
+
 test('manage dashboard', async ({ page, token }) => {
   const testID = crypto.randomUUID();
 
   await setAuth({page, token});
 
   await createBoard({page, testID});
+
+  await deleteBoard({page, testID});
 
   await page.waitForTimeout(3000);
 });
