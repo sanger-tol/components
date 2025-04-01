dabda6534995360ae85b2c448dca50a9117858ba
Edward Moulsdale <em29@sanger.ac.uk>
2025-04-01 10:32:16 +0100

    remoe

--- test/playwright/playwright.config.ts
+++ test/playwright/playwright.config.ts
@@ -41,9 +41,9 @@ export default defineConfig({
     ignoreHTTPSErrors: true,
   },
   expect: {
-    timeout: 5000,
+    timeout: 5_000,
   },
-  timeout: 10_000,
+  timeout: 25_000,
 
   /* Configure projects for major browsers */
   projects: [
