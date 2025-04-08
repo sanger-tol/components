1330f4d365c382e8d8047d03739c9988cfe558f4
Luke Wilson <lw23@sanger.ac.uk>
2025-04-08 14:41:03 +0100

    re-order

--- components-ui/app/src/pages/Factories.tsx
+++ components-ui/app/src/pages/Factories.tsx
@@ -9,10 +9,7 @@ import { CodeBlock } from "react-code-blocks";
 import { truncateString } from "../tol-ui/src/general/utils";
 
 function Factories() {
-  const docsTitle = <h2>Factories</h2>;
-
   const words = createTextGeneratorFactory();
-
   const fiveWords = words.generateWords(5);
   const twoSentences = words.generateSentences(2);
   const oneParagraph = words.generateParagraphs(1);
@@ -22,7 +19,7 @@ function Factories() {
     paragraphUpperBound: 4,
     sentenceLowerBound: 2,
     sentenceUpperBound: 4,
-    words: ["fizz", "buzz", "hullabaloo", "foo", "bar", ],
+    words: ["fizz", "buzz", "hullabaloo", "foo", "bar"],
   });
   const fizzBuzzFooBar = fizzBuzzFooBarFactory.generateParagraphs(1);
 
@@ -45,46 +42,49 @@ function Factories() {
     "  words: ['fizz', 'buzz', 'hullabaloo', 'foo', 'bar'], // words to use instead of fake Latin\n" +
     "});\n\n" +
     "const fizzBuzzFooBar = words.generateParagraph(2);\n" +
-    `// ${truncateString(fizzBuzzFooBar, 200)}\n`
+    `// ${truncateString(fizzBuzzFooBar, 200)}\n`;
+
+  const docsTitle = <h2>Factories</h2>;
 
   const content = (
     <>
-    <div>
-      <h3>Text Generator Factory</h3>
-      <p>Generate words, sentences or paragraphs to add to add components.</p>
-      <p style={{marginBottom: "20px"}}>
-        This is useful for generating random text to fill out components when
-        developing, especially useful for testing larger amounts of text on smaller screens.
-      </p>
-    </div>
-    <div>
-      <h5 style={{ marginBottom: "5px" }}>Initialise factory:</h5>
-      <div className="tol-download-modal-code">
-        <CodeBlock
-          text={createFactory}
-          language="javascript"
-          showLineNumbers={false}
-        />
+      <div>
+        <h3>Text Generator Factory</h3>
+        <p>Generate words, sentences or paragraphs to add to add components.</p>
+        <p style={{ marginBottom: "20px" }}>
+          This is useful for generating random text to fill out components when
+          developing, especially useful for testing larger amounts of text on
+          smaller screens.
+        </p>
       </div>
-      <h5 style={{ margin: "5px 0px" }}>
-        Generate words, sentences, or paragraphs:
-      </h5>
-      <div className="tol-download-modal-code">
-        <CodeBlock
-          text={codeBlock}
-          language="javascript"
-          showLineNumbers={false}
-        />
+      <div>
+        <h5 style={{ marginBottom: "5px" }}>Initialise factory:</h5>
+        <div className="tol-download-modal-code">
+          <CodeBlock
+            text={createFactory}
+            language="javascript"
+            showLineNumbers={false}
+          />
+        </div>
+        <h5 style={{ margin: "5px 0px" }}>
+          Generate words, sentences, or paragraphs:
+        </h5>
+        <div className="tol-download-modal-code">
+          <CodeBlock
+            text={codeBlock}
+            language="javascript"
+            showLineNumbers={false}
+          />
+        </div>
+        <h5 style={{ margin: "5px 0px" }}>Add options to the factory:</h5>
+        <div className="tol-download-modal-code">
+          <CodeBlock
+            text={addOptions}
+            language="javascript"
+            showLineNumbers={false}
+          />
+        </div>
       </div>
-      <h5 style={{ margin: "5px 0px" }}>Add options to the factory:</h5>
-      <div className="tol-download-modal-code">
-        <CodeBlock
-          text={addOptions}
-          language="javascript"
-          showLineNumbers={false}
-        />
-      </div>
-    </div>
     </>
   );
 
