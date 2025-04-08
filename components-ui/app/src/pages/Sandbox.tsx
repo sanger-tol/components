b453b76aa8499f0f68457fa64442048a5e97853c
Logan Howat <lh31@sanger.ac.uk>
2025-04-08 11:32:58 +0100

    messing around with sandbox

--- components-ui/app/src/pages/Sandbox.tsx
+++ components-ui/app/src/pages/Sandbox.tsx
@@ -4,8 +4,23 @@ SPDX-FileCopyrightText: 2023 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
+import { RemoteSunburst } from "../tol-ui/src";
+
 function Sandbox() {
-  return <></>;
+
+  const speciesSunburst = (
+    <RemoteSunburst
+      id="home-species-sunburst-v1"
+      endpoint="species"
+      title="Species"
+      sliceBy={["sts_order_group", "sts_family"]}
+      legendPosition="left"
+    />
+  );
+
+  return (<>
+    {speciesSunburst}
+  </>);
 }
 
 export default Sandbox;
