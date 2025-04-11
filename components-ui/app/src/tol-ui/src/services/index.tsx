53104801f565a150fa3f973f6ad717d470a18724
Kiernan Harding <kh16@sanger.ac.uk>
2025-04-11 14:19:15 +0100

    fix

--- components-ui/app/src/tol-ui/src/services/index.tsx
+++ components-ui/app/src/tol-ui/src/services/index.tsx
@@ -3,5 +3,4 @@
 // SPDX-License-Identifier: MIT
 
 export { httpClient } from "./http/httpClient";
-export { default as TsDataSource } from "./datasource/TsDataSource";
 export { default as DetailAttribute } from "./http/components/DetailAttribute";
