0ecbc57afc61952a9b3ac5eb0619372b544d45a5
Logan Howat <lh31@sanger.ac.uk>
2025-04-14 08:47:12 +0100

    Removing uneeded imports

--- components-ui/app/src/tol-ui/src/models/General.ts
+++ components-ui/app/src/tol-ui/src/models/General.ts
@@ -4,19 +4,10 @@ SPDX-FileCopyrightText: 2025 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-import { IButton } from "../general/Button";
-import { IDropdownButtons } from "../general/DropdownButtons";
-
 export interface IInlineEdit {
   title: string;
   editable?: boolean;
   onSave?: (value: string) => void;
   onChange?: (value: string) => void;
-}
-
-export interface IUtilityBar {
-  title?: IInlineEdit;
-  buttons?: (IButton | IDropdownButtons | undefined)[];
-  elements?: JSX.Element[];
 }
 
