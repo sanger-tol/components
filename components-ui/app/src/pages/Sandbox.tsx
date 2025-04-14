2146113bd703d8a1904d5a390b0c36986ebe80e7
Logan Howat <lh31@sanger.ac.uk>
2025-04-14 08:55:19 +0100

    Removing sandbox page

--- components-ui/app/src/pages/Sandbox.tsx
+++ components-ui/app/src/pages/Sandbox.tsx
@@ -4,23 +4,9 @@ SPDX-FileCopyrightText: 2023 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-import { RemoteSunburst } from "../tol-ui/src";
-
 function Sandbox() {
 
-  const speciesSunburst = (
-    <RemoteSunburst
-      id="home-species-sunburst-v1"
-      endpoint="species"
-      title="Species"
-      sliceBy={["sts_order_group", "sts_family"]}
-      legendPosition="left"
-    />
-  );
-
-  return (<>
-    {speciesSunburst}
-  </>);
+  return <></>;
 }
 
 export default Sandbox;
