17652db622317add34f5e344b2fd3ad6b41022cb
Logan Howat <lh31@sanger.ac.uk>
2025-04-16 15:10:01 +0100

    Fixing build issues

--- components-ui/app/src/tol-ui/src/models/index.ts
+++ components-ui/app/src/tol-ui/src/models/index.ts
@@ -4,7 +4,6 @@ SPDX-FileCopyrightText: 2022 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-export { HeaderButton } from "./HeaderButton";
 export { Page, Dropdown } from "./Nav";
 export { User } from "./User";
 export { IFilter } from "./Filter";
