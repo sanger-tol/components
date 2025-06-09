fae8a225607790fcc7453575e5d820065a1ae431
Kiernan Harding <kh16@sanger.ac.uk>
2025-06-09 09:50:06 +0100

    temp back-up

--- components-ui/app/src/tol-ui/src/boards/my-boards/Accordion.tsx
+++ components-ui/app/src/tol-ui/src/boards/my-boards/Accordion.tsx
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: MIT
  */
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Accordion as Acc } from "rsuite";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChartSimple, faTable } from "@fortawesome/free-solid-svg-icons";
@@ -12,7 +12,6 @@ import { useHistory } from "react-router-dom";
 import {
   IDropdownButtonConfig,
   IDropdownMainIconProps,
-  httpClient,
   TsDataSource,
   AccordionHeader,
   DropdownButtons,
@@ -50,135 +49,6 @@ interface ZonesAccordionProps {
 interface ComponentsProps {
   componentIds: string[];
 }
-
-// All of these functions will need to be replaced with TsDataSource ones
-// When all MRs have been merged into dev
-const returnViewInfo = async (viewId: string) => {
-  try {
-    const res: any = await httpClient().get(`/${BOARDS.VIEW}`, {
-      params: {
-        filter: {
-          and_: {
-            id: { eq: { value: viewId } },
-          },
-        },
-      },
-    });
-    return res.data.data[0].attributes.title;
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-};
-
-const returnZoneInfo = async (zoneId: string) => {
-  try {
-    const res: any = await httpClient().get(`/${BOARDS.ZONE}`, {
-      params: {
-        filter: {
-          and_: {
-            id: { eq: { value: zoneId } },
-          },
-        },
-      },
-    });
-    return res.data.data.map((item: any) => ({
-      title: item.attributes.title,
-      objectType: item.attributes.object_type,
-    }));
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-};
-
-const returnComponentInfo = async (componentId: string) => {
-  try {
-    const res: any = await httpClient().get(`/${BOARDS.COMPONENT}`, {
-      params: {
-        filter: {
-          and_: {
-            id: { eq: { value: componentId } },
-          },
-        },
-      },
-    });
-    return res.data.data.map((item: any) => ({
-      title: item.attributes.title,
-      componentType: item.attributes.component_type,
-    }));
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-};
-
-const fetchSubItemId = async (
-  id: string,
-  endpointUrl?: string,
-  filterItem?: string,
-  itemType?: any,
-) => {
-  try {
-    const res: any = await httpClient().get(`/${endpointUrl}`, {
-      params: {
-        filter: {
-          and_: {
-            [`${filterItem}`]: { eq: { value: id } },
-          },
-        },
-      },
-    });
-    return res.data.data.map((item: any) => ({
-      id: item.relationships[itemType].data.id,
-      order: item.attributes.order,
-    }));
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-};
-
-const useItemData = <T,>(
-  ids: string[],
-  fetchFunction: (id: string) => Promise<T> | T,
-) => {
-  const [itemData, setItemData] = useState<{ [key: string]: T }>({});
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    let mounted = true;
-
-    const fetchItems = async () => {
-      setLoading(true);
-
-      try {
-        const results = await Promise.all(
-          ids.map(async (id) => {
-            const data = await fetchFunction(id);
-            return { [id]: data };
-          }),
-        );
-
-        if (mounted) {
-          setItemData(Object.assign({}, ...results));
-        }
-      } finally {
-        if (mounted) {
-          setLoading(false);
-        }
-      }
-    };
-
-    fetchItems();
-
-    return () => {
-      mounted = false;
-    };
-  }, [ids, fetchFunction]);
-
-  return { itemData, loading };
-};
 
 export function Accordion(props: BoardsAccordionProps) {
   const { boardDetails, setBoardDetails } = props;
