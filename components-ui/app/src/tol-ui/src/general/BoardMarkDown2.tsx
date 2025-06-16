c327aa3383cc6de983bf7bb04fa0bd6477339c5b
Nithin <nj9@sanger.ac.uk>
2025-06-16 14:40:28 +0100

    Markdown toolbar feature addition.

--- components-ui/app/src/tol-ui/src/general/BoardMarkDown2.tsx
+++ components-ui/app/src/tol-ui/src/general/BoardMarkDown2.tsx
@@ -7,23 +7,25 @@ SPDX-License-Identifier: MIT
 import { useState, useEffect } from "react";
 import Markdown from "./Markdown";
 import MDEditor from "@uiw/react-md-editor";
+import rehypeSanitize from "rehype-sanitize";
 import { IButton } from "./Button";
 import { UtilityBar, TsDataSource } from "../index";
 import { saveTitle, upsertComponentConfig } from "../boards/utils";
 import { BoardObjectTypes } from "../constants";
 
-export interface IMdComponentConfig {
+
+export interface IMarkdownConfig {
   content: string;
 }
 
 interface Props {
-  config: IMdComponentConfig;
+  config: IMarkdownConfig;
   id: string;
   size: string;
   title: string;
 }
 
-const RESOLUTION = { sm: "90px", md: "405px", lg: "565px" };
+// const RESOLUTION = { sm: "90px", md: "300px", lg: "565px" };
 
 function BoardMarkdown(props: Props) {
   const { config, id, size, title } = props;
@@ -32,41 +34,43 @@ function BoardMarkdown(props: Props) {
   const [flipped, setFlipped] = useState<boolean>(false);
   const [content, setContent] = useState<string>(config.content || "");
   const [showPreview, setShowPreview] = useState<boolean>(false);
-  const [showMarkdown, setShowMarkdown] = useState<boolean>(false);
+  const [showMarkdownViewer, setShowMarkdownViewer] = useState<boolean>(false);
+
 
   useEffect(() => {
-    config.content && setShowMarkdown(true);
+    config.content && setShowMarkdownViewer(true);
   }, []);
 
-  const onMarkdownSave = (config: IMdComponentConfig) => {
-    {
-      showMarkdown === false && upsertComponentConfig(ds, id, { ...config });
+  const onMarkdownSave = (config: IMarkdownConfig) => {
+    if (!showMarkdownViewer) {
+      upsertComponentConfig(ds, id, { ...config });
     }
-  };
+  }
 
   const PreviewButton: IButton = {
     position: "right",
     type: "primary",
     icon: showPreview ? "eye-slash" : "eye",
     onClick: () => setShowPreview(!showPreview),
-  };
+    visible: !showMarkdownViewer,
+  }
 
   const EditButton: IButton = {
     position: "right",
     type: "primary",
-    tooltip: showMarkdown ? "Edit" : "Save",
-    icon: showMarkdown ? "edit" : "save",
+    tooltip: showMarkdownViewer ? "Edit" : "Save",
+    icon: showMarkdownViewer ? "edit" : "save",
     onClick: () => {
-      setShowMarkdown(!showMarkdown);
+      setShowMarkdownViewer(!showMarkdownViewer);
       onMarkdownSave({ content: content });
       setFlipped(!flipped);
     },
-  };
+  }
 
   const MdUtilityBar = (
     <UtilityBar
       id="editor-markdown"
-      buttons={[EditButton, !showMarkdown ? PreviewButton : undefined]}
+      buttons={[EditButton, PreviewButton]}
       title={{
         title: title,
         editable: true,
@@ -78,36 +82,29 @@ function BoardMarkdown(props: Props) {
   );
 
   const MarkdownEditor = (
-    <div className="tol-rich-text-front">
-      <MDEditor
-        value={content}
-        onChange={(content?: string) => setContent(content ?? "")}
-        preview={showPreview ? "live" : "edit"}
-        height={RESOLUTION[size]}
-      />
-    </div>
+    <MDEditor
+      value={content}
+      onChange={(content?: string) => setContent(content ?? "")}
+      preview={showPreview ? "live" : "edit"}
+      previewOptions={{
+        rehypePlugins: [[rehypeSanitize]],
+      }}
+      hideToolbar={size === 'sm'}
+      height="100%"
+    />
   );
 
   const MarkdownViewer = (
-    <div className="tol-rich-text-back">
-      <div className={`tol-rich-text-viewer-inner-container ${size}`}>
-        <Markdown contents={content} />
-      </div>
-    </div>
+    <Markdown contents={content} />
   );
 
   return (
-    <div>
+    <>
       {MdUtilityBar}
-      <div className="tol-rich-text-flip-container">
-        <div
-          className={`tol-rich-text-flipper ${showMarkdown ? "flipped" : ""}`}
-        >
-          {MarkdownEditor}
-          {MarkdownViewer}
-        </div>
+      <div className="tol-component-contents tol-markdown">
+        {showMarkdownViewer ? MarkdownViewer : MarkdownEditor}
       </div>
-    </div>
+    </>
   );
 }
 
