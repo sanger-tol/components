a298af05bc3b9b61636f36e54e9032d5bbbf3bc2
Nithin <nj9@sanger.ac.uk>
2025-07-03 13:31:00 +0100

    Lynting

--- components-ui/app/src/tol-ui/src/constants/timeline.constants.ts
+++ components-ui/app/src/tol-ui/src/constants/timeline.constants.ts
@@ -5,10 +5,10 @@ SPDX-License-Identifier: MIT
 */
 
 export const TIMELINE_DOTS = {
-ACTIVE_DOT: "active-dot",
-DOT : "dot",
-DOT_DEFAULT : "#d9d9d9",
-ACTIVE_DOT_DEFAULT : "#039be5",
-DEFAULT_ICON_COLOR : "#15b215",
-DEFAULT_NOT_DEFINED : "#fff",
-}
+  ACTIVE_DOT: "active-dot",
+  DOT: "dot",
+  DOT_DEFAULT: "#d9d9d9",
+  ACTIVE_DOT_DEFAULT: "#039be5",
+  DEFAULT_ICON_COLOR: "#15b215",
+  DEFAULT_NOT_DEFINED: "#fff",
+};
