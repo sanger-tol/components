6efd310546aa5951c50f2486cac93a5bd2d8e3a1
Kiernan Harding <kh16@sanger.ac.uk>
2025-07-08 22:33:06 +0100

    alembic

--- components-api/migrations/alembic/versions/26fd2f3fa577_update_id_fields_in_boards.py
+++ components-api/migrations/alembic/versions/26fd2f3fa577_update_id_fields_in_boards.py
@@ -7,6 +7,7 @@ Create Date: 2025-07-08 20:36:10.083991
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.sql import text
 
 
 # revision identifiers, used by Alembic.
@@ -17,7 +18,15 @@ depends_on = None
 
 
 def upgrade() -> None:
-    pass
+    conn = op.get_bind()
+    # update the config field in the components table
+    conn.execute(
+        text("""
+        UPDATE components
+        SET config = jsonb_replace(config::jsonb, '"uid"', '"id"')
+        WHERE config::text LIKE '%"uid"%'
+        """)
+    )
 
 
 def downgrade() -> None:
