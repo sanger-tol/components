379163c101a17a986d83727a9d1e744e6e726108
Kiernan Harding <kh16@sanger.ac.uk>
2025-07-08 21:44:31 +0100

    alembic

---
+++ components-api/migrations/alembic/versions/26fd2f3fa577_update_id_fields_in_boards.py
@@ -0,0 +1,24 @@
+"""update id fields in boards
+
+Revision ID: 26fd2f3fa577
+Revises: 05ddf4259643
+Create Date: 2025-07-08 20:36:10.083991
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '26fd2f3fa577'
+down_revision = '05ddf4259643'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
