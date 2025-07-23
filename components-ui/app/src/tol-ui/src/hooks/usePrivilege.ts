d81f6b4a2e8ba01e4d087d4644d64d3135ef5c46
Logan Howat <lh31@sanger.ac.uk>
2025-07-23 16:24:41 +0100

    Fixing build issues

--- components-ui/app/src/tol-ui/src/hooks/usePrivilege.ts
+++ components-ui/app/src/tol-ui/src/hooks/usePrivilege.ts
@@ -5,10 +5,10 @@ SPDX-License-Identifier: MIT
 */
 
 import { useContext } from "react";
-import { PrivilegeContext, BoardPrivilegeContextValue } from "..";
+import { PrivilegeContext, IBoardPrivilegeContextValue } from "..";
 
 export const useBoardPrivilege = () => {
-    const context = useContext<BoardPrivilegeContextValue | undefined>(PrivilegeContext);
+    const context = useContext<IBoardPrivilegeContextValue | undefined>(PrivilegeContext);
     if (context === undefined) {
         // returns undefined for components that are not in a board
         return { privilege: undefined, setPrivilege: () => { } };
