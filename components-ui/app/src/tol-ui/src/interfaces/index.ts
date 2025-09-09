df2c0ae38d189c65c76984a1024c083f3230f897
Nithin <nj9@sanger.ac.uk>
2025-09-09 10:22:13 +0100

    small bit

--- components-ui/app/src/tol-ui/src/interfaces/index.ts
+++ components-ui/app/src/tol-ui/src/interfaces/index.ts
@@ -21,4 +21,4 @@ export * from "./Markdown";
 export * from "./Widgets";
 export * from "./Drawer";
 export * from "./FileValidation";
-export * from "./SocialViewer"
+export * from "./SocialViewer";
