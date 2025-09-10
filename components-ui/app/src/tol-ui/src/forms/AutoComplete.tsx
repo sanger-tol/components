531d48914d49e065f49cef5d741c11e7484f878d
Luke Wilson <lw23@sanger.ac.uk>
2025-09-10 14:57:56 +0100

    Removed redundant code

--- components-ui/app/src/tol-ui/src/forms/AutoComplete.tsx
+++ components-ui/app/src/tol-ui/src/forms/AutoComplete.tsx
@@ -35,9 +35,8 @@ export function AutoComplete(props: PAutoComplete) {
     displayFieldsTitle,
     loading,
     errorText,
+    icon,
   } = props;
-
-  const icon = { ...props.icon, position: props.icon?.position || "right" };
 
   return (
     <RSForm.Group controlId={label}>
