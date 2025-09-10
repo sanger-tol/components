4bc2c0738579834c32db618376f0ae890dee66ae
Luke Wilson <lw23@sanger.ac.uk>
2025-09-10 14:54:22 +0100

    Added the ability to have icons in form components

--- components-ui/app/src/pages/Sandbox.tsx
+++ components-ui/app/src/pages/Sandbox.tsx
@@ -4,6 +4,32 @@ SPDX-FileCopyrightText: 2023 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
+import { SocialViewer } from "../tol-ui/src/general/SocialViewer";
+
+const data = [
+  {
+    name: "Luke",
+    links: [
+      {
+        link: "https://linkedin.com/in/luke",
+        icon: "linkedin",
+      },
+    ],
+  },
+  {
+    name: "Nithin",
+    links: [
+      {
+        link: "https://linkedin.com/in/nithin",
+        icon: "linkedin",
+      },
+    ],
+  },
+];
 export function Sandbox() {
-  return <></>;
+  return (
+    <div style={{ padding: "10px" }}>
+      <SocialViewer data={data} />
+    </div>
+  );
 }
