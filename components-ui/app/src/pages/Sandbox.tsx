ca9ea80113d8c03251155f82f086109b293b449f
Luke Wilson <lw23@sanger.ac.uk>
2025-09-10 14:55:38 +0100

    Delete Sandbox Component

--- components-ui/app/src/pages/Sandbox.tsx
+++ components-ui/app/src/pages/Sandbox.tsx
@@ -4,32 +4,6 @@ SPDX-FileCopyrightText: 2023 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-import { SocialViewer } from "../tol-ui/src/general/SocialViewer";
-
-const data = [
-  {
-    name: "Luke",
-    links: [
-      {
-        link: "https://linkedin.com/in/luke",
-        icon: "linkedin",
-      },
-    ],
-  },
-  {
-    name: "Nithin",
-    links: [
-      {
-        link: "https://linkedin.com/in/nithin",
-        icon: "linkedin",
-      },
-    ],
-  },
-];
 export function Sandbox() {
-  return (
-    <div style={{ padding: "10px" }}>
-      <SocialViewer data={data} />
-    </div>
-  );
+  return <></>;
 }
