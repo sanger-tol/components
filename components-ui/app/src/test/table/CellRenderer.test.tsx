2fbc465f5dbd1a6a7fde08a67af0095239147d52
Logan Howat <lh31@sanger.ac.uk>
2025-09-16 13:42:06 +0100

    Starting tests for CellFilterResult

---
+++ components-ui/app/src/test/table/CellRenderer.test.tsx
@@ -0,0 +1,19 @@
+/*
+SPDX-FileCopyrightText: 2024 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+*/
+
+import { expect, test, describe, vitest } from "vitest";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { useState, act } from "react";
+import {
+  CellFilterResult,
+} from "../../tol-ui/src";
+
+
+describe("Testing CellFilterResult function", () => {
+  test("Resets zone correctly", () => {
+    });
+});
