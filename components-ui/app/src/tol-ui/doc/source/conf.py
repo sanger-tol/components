877513dde990e987a05a3ff45705631a51d34c4d
Andrew Varley <av10@sanger.ac.uk>
2025-09-18 10:41:59 +0100

    TOLP-9114 Auto-documentation

---
+++ components-ui/app/src/tol-ui/doc/source/conf.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2025 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+
+project = 'Tree of Life SDK'
+copyright = '2025 Genome Research Ltd'  # noqa A001
+author = 'ToL Enabling Platforms Team'
+# release = '1.6.30'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx_js']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+js_language = 'typescript'
+js_source_path = 'src'
