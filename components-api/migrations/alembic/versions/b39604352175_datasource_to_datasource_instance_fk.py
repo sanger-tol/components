233bb034f0c508d9f87b4cc7a1e81102ae22e077
Thomas Assheton <ta21@sanger.ac.uk>
2025-09-19 15:05:58 +0100

    Create migration

---
+++ components-api/migrations/alembic/versions/b39604352175_datasource_to_datasource_instance_fk.py
@@ -0,0 +1,24 @@
+"""datasource_to_datasource_instance_fk
+
+Revision ID: b39604352175
+Revises: 5698bc61eed0
+Create Date: 2025-09-19 14:00:17.250971
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b39604352175'
+down_revision = '5698bc61eed0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
