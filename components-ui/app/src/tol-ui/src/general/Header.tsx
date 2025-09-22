274adcc9f71a4ded952ef6a8b3afa50da3cba207
Kiernan Harding <kh16@sanger.ac.uk>
2025-09-22 15:28:27 +0100

    so nearly working!

--- components-ui/app/src/tol-ui/src/general/Header.tsx
+++ components-ui/app/src/tol-ui/src/general/Header.tsx
@@ -4,30 +4,37 @@ SPDX-FileCopyrightText: 2022 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 
 export interface PHeader {
   title?: string;
   subTitle?: string;
   element?: ReactNode;
-  height?: number;
-  backgroundImageLink?: string;
+  image?: string;
+  video?: string;
+  fade?: number;
+  landingPage?: boolean;
 }
 
 export function Header(props: PHeader) {
-  const { title, subTitle, element, height, backgroundImageLink } = props;
+  const { title, subTitle, element, landingPage, image, video } = props;
 
   return (
     <>
       <header
-        className="masthead text-white"
+        className="masthead"
         style={{
-          backgroundImage: backgroundImageLink ? `url(${backgroundImageLink})` : undefined,
-          height: height ? height : undefined
+          height: landingPage ? "100vh" : undefined
         }}
       >
-        <div className="masthead-content">
-          {!backgroundImageLink &&
+        {image && <img className="masthead-media" src={image} alt="background" />}
+        {video && (
+          <video className="masthead-media" autoPlay loop muted>
+            <source src={video} type="video/mp4" />
+          </video>
+        )}
+        <div className="masthead-default">
+          {!image && !video &&
             <>
               <li></li>
               <li></li>
