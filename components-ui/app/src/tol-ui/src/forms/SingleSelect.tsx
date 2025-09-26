31dce19160fee57e7b02f0b45e72b2c0d7b4ef9f
Thomas Assheton <ta21@sanger.ac.uk>
2025-09-26 10:31:41 +0100

    Exposed the `disabled` prop of rsuite's SelectPicker in SingleSelect, and also added disabled tooltips to SingleSelect.

--- components-ui/app/src/tol-ui/src/forms/SingleSelect.tsx
+++ components-ui/app/src/tol-ui/src/forms/SingleSelect.tsx
@@ -5,32 +5,51 @@ SPDX-License-Identifier: MIT
 */
 
 import { useEffect, useState } from "react";
-import { SelectPicker } from "rsuite";
+import { Whisper, SelectPicker, Tooltip } from "rsuite";
 
 export interface PSingleSelect {
   data: string[];
   placeholder?: string;
   value: string;
   setValue: any;
+  disabled?: boolean;
+  disabledTooltip?: string;
   block?: boolean;
 }
 
 export const SingleSelect = (props: PSingleSelect) => {
-  const { placeholder, setValue, value, block } = props;
+  const { placeholder, setValue, value, disabledTooltip, block } = props;
+  
+  let { disabled } = props;
+  if (!disabled) {
+    // Convert `disabled` from a falsy value to explicitly `false`
+    // This is required for disabled props in components below
+    disabled = false;
+  }
+ 
   const [data, setData] = useState([{}]);
 
   useEffect(() => {
     setData(props.data.map((item) => ({ label: item, value: item })));
   }, [props.data]);
 
+  const DisabledTooltip = (
+    <Tooltip>{disabledTooltip}</Tooltip>
+  );
+
   return (
-    <SelectPicker
-      data={data}
-      searchable={false}
-      value={value}
-      onChange={setValue}
-      placeholder={placeholder}
-      block={block}
-    />
+    <Whisper open={disabled} speaker={DisabledTooltip}>
+      <span>
+        <SelectPicker
+          data={data}
+          searchable={false}ß
+          value={value}
+          onChange={setValue}
+          placeholder={placeholder}
+          disabled={disabled}ß
+          block={block}
+        />
+      </span>
+    </Whisper>
   );
 };
