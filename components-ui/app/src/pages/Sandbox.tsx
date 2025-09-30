1a796271002a10ee23c770ac90d9de8edd97c676
Nithin <nj9@sanger.ac.uk>
2025-09-30 12:25:49 +0100

    sandbox env deletion

--- components-ui/app/src/pages/Sandbox.tsx
+++ components-ui/app/src/pages/Sandbox.tsx
@@ -4,12 +4,6 @@ SPDX-FileCopyrightText: 2025 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-import { VideoPlayer } from "../tol-ui/src";
-
 export function Sandbox() {
-  return (
-    <>
-      <VideoPlayer videoId="SqcY0GlETPk" />
-    </>
-  );
+  return <></>;
 }
