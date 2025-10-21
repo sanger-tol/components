9ff9b49cf8095cab2a3a0d33fb00db11b4e16d1b
Kiernan Harding <kh16@sanger.ac.uk>
2025-10-21 11:20:10 +0100

    TOLP-9176: Link updates and tab styling

--- components-ui/app/src/tol-ui/src/table/cellRenderers/Link.tsx
+++ components-ui/app/src/tol-ui/src/table/cellRenderers/Link.tsx
@@ -20,22 +20,33 @@ export interface PLink extends PCell {
 
 export function Link(props: PLink) {
   const { value, url, text, buttonConfig } = props;
-
   const t = text || value;
+
+  const isExternal = () => {
+    return /^(https?:\/\/|www\.|[a-zA-Z0-9-]+\.[a-zA-Z]{2,})/.test(url);
+  };
+
+  const handleClick = () => {
+    if (isExternal()) {
+      window.open(url, "_blank", "noopener,noreferrer");
+    } else {
+      window.location.href = url;
+    }
+  };
 
   if (buttonConfig) {
     return (
       <Button
         text={t}
-        onClick={() => window.open(url, "_blank")}
+        onClick={handleClick}
         {...buttonConfig}
       />
     );
   }
 
-  return (
-    <a href={url} target="_blank" rel="noopener noreferrer">
-      {t}
-    </a>
+  return isExternal() ? (
+    <a href={url} target="_blank" rel="noopener noreferrer">{t}</a>
+  ) : (
+    <a href={url}>{t}</a>
   );
 }
