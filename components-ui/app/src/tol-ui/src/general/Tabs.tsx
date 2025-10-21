2265823aa8e87c6e390f752f90fb777d40eeb8e8
Kiernan Harding <kh16@sanger.ac.uk>
2025-10-21 09:42:12 +0100

    tol tabs

---
+++ components-ui/app/src/tol-ui/src/general/Tabs.tsx
@@ -0,0 +1,11 @@
+/*
+SPDX-FileCopyrightText: 2025 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+*/
+
+import { Tabs as RSTabs } from "rsuite";
+
+export function Tabs(props: any) {
+  return <RSTabs appearance="pills" {...props} />;
+}
