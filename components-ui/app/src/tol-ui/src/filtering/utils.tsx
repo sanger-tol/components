6b8b3ebed72bceda8e014380afe4bd6e020220c4
Kiernan Harding <kh16@sanger.ac.uk>
2025-10-23 15:59:22 +0100

    go

--- components-ui/app/src/tol-ui/src/filtering/utils.tsx
+++ components-ui/app/src/tol-ui/src/filtering/utils.tsx
@@ -12,6 +12,7 @@ import {
   IAndAttributes,
   deepCopy,
   isEmptyObject,
+  deepEqual,
 } from "..";
 
 
@@ -405,23 +406,4 @@ export function isFiltersEqual(filter1: object, filter2: object): boolean {
   } catch {
     return false;
   }
-}
-
-function deepEqual(a: any, b: any): boolean {
-  if (a === b) return true;
-
-  if (typeof a !== 'object' || typeof b !== 'object' || a === null || b === null)
-    return false;
-
-  const keysA = Object.keys(a);
-  const keysB = Object.keys(b);
-
-  if (keysA.length !== keysB.length) return false;
-
-  for (const key of keysA) {
-    if (!keysB.includes(key)) return false;
-    if (!deepEqual(a[key], b[key])) return false;
-  }
-
-  return true;
 }
--- components-ui/app/src/tol-ui/src/general/utils.tsx
+++ components-ui/app/src/tol-ui/src/general/utils.tsx
@@ -300,3 +300,22 @@ export function sortObjectAlphabetically(obj: Record<string, any>): Record<strin
       return sortedObj;
     }, {});
 }
+
+export function deepEqual(a: any, b: any): boolean {
+  if (a === b) return true;
+
+  if (typeof a !== 'object' || typeof b !== 'object' || a === null || b === null)
+    return false;
+
+  const keysA = Object.keys(a);
+  const keysB = Object.keys(b);
+
+  if (keysA.length !== keysB.length) return false;
+
+  for (const key of keysA) {
+    if (!keysB.includes(key)) return false;
+    if (!deepEqual(a[key], b[key])) return false;
+  }
+
+  return true;
+}
