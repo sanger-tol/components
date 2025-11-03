9eb5e3d9f7c638497b686adda56e34e37d282fe3
Logan Howat <lh31@sanger.ac.uk>
2025-11-03 13:32:50 +0000

    New alembic upgrade to add action_name and make flow_name nullable

--- components-api/app/main/model/action.py
+++ components-api/app/main/model/action.py
@@ -21,7 +21,8 @@ class Action(Base):
     name: Mapped[str] = mapped_column(nullable=False)
     object_type: Mapped[str] = mapped_column(nullable=False)
 
-    flow_name: Mapped[str] = mapped_column(nullable=False)
+    flow_name: Mapped[str] = mapped_column(nullable=True)
+    action_name: Mapped[str] = mapped_column(nullable=True)
     params: Mapped[dict] = mapped_column(
         JSONB,
         nullable=False,
