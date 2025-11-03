69fe963a3974661de3ab66acfd1973d7323b74b0
Nithin <nj9@sanger.ac.uk>
2025-11-03 13:47:51 +0000

    Placeholder component added to the Videoplayer for empty videoss

--- components-ui/app/src/tol-ui/src/general/VideoPlayer.tsx
+++ components-ui/app/src/tol-ui/src/general/VideoPlayer.tsx
@@ -4,15 +4,42 @@ SPDX-FileCopyrightText: 2025 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
+import { Placeholder } from "..";
+
 export interface PVideoPlayer {
   host?: "youtube" | "vimeo";
-  videoId: string;
+  videoId?: string;
   width?: string | number;
   height?: string | number;
 }
 
 export function VideoPlayer(props: PVideoPlayer) {
   let { host = "youtube", videoId, width, height } = props;
+
+  if (!videoId) {
+    if (!width && !height) {
+      width = "300px";
+      height = `${Number(width.replace("px", "")) * (9 / 16)}px`;
+    }
+    if (height && !width) {
+      if (typeof height === "string") {
+        width = `${Number(height.replace("px", "")) * (16 / 9)}px`;
+      } else {
+        width = Number(height) * (16 / 9);
+      }
+    }
+    return (
+      <div
+        style={{
+          width: width,
+          border: "2px solid var(--tol-grey-subtle)",
+          borderRadius: "6px",
+        }}
+      >
+        <Placeholder message={"Video coming soon..."} height={height} />
+      </div>
+    );
+  }
 
   let url = `https://www.youtube.com/embed/${videoId}`;
 
@@ -23,9 +50,18 @@ export function VideoPlayer(props: PVideoPlayer) {
   }
 
   if (!height && width) {
-    height = Number(width) * (9 / 16);
+    if (typeof width === "string") {
+      height = `${Number(width.replace("px", "")) * (9 / 16)}px`;
+    } else {
+      height = Number(width) * (9 / 16);
+    }
   } else if (!width && height) {
-    width = Number(height) * (16 / 9);
+    if (typeof height === "string") {
+    width = `${Number(height.replace("px", "")) * (16 / 9)}px`;
+    }
+    else{
+      width = Number(height) * (16 / 9);
+    }
   }
 
   return (
