d39480c15bd6615c65380332f6c672caf53de8ee
Thomas Assheton <ta21@sanger.ac.uk>
2025-11-10 11:55:54 +0000

    Write initial unit test

---
+++ components-ui/app/src/test/attributes/AttributeUtils.test.tsx
@@ -0,0 +1,28 @@
+/*
+SPDX-FileCopyrightText: 2025 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+*/
+
+import { expect, test, describe } from "vitest";
+import {
+  getReadOnlyAndFilterText,
+  IFilterOperatorOptions,
+  TFilterOperatorType
+} from "../../tol-ui/src";
+
+describe("getReadOnlyAndFilterText function", () => {
+  test("Direct value operator generates correct prose", () => {
+    const operator: [TFilterOperatorType, IFilterOperatorOptions] = [
+      "eq",
+      {
+        value: "Abrostola",
+        negate: false,
+      }
+    ];
+
+    const generatedProse = getReadOnlyAndFilterText(operator);
+
+    expect(generatedProse).toBe("must be equal to Abrostola");
+  })
+});
