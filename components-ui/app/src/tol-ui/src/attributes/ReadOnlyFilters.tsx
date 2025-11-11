71240a440441df7a69199ede9953ff31261c9709
Thomas Assheton <ta21@sanger.ac.uk>
2025-11-11 11:13:06 +0000

    Add list of descriptions, calling previously made function

--- components-ui/app/src/tol-ui/src/attributes/ReadOnlyFilters.tsx
+++ components-ui/app/src/tol-ui/src/attributes/ReadOnlyFilters.tsx
@@ -1,9 +1,25 @@
 import { IFilter, IRemoteTarget } from "src/interfaces";
+import { AttributeTitle } from "./AttributeTitle";
+import { generateFilterDescriptions } from "./utils";
 
 interface PReadOnlyFilters extends IRemoteTarget {
-    filter: IFilter
+  filter: IFilter
 }
 
 export function ReadOnlyFilters(props: PReadOnlyFilters) {
-    const { filter, objectType, dataSource } = props;
+  const { filter, objectType, dataSource } = props;
+
+  const filterDescriptions = generateFilterDescriptions(filter);
+
+  return (
+    <div>
+      {filterDescriptions && Object.entries(filterDescriptions).map(([attribute, prose]) => (
+        <AttributeTitle
+          objectType={objectType}
+          dataSource={dataSource}
+          field={`${attribute} ${prose}`}
+        />
+      ))}
+    </div>
+  )
 }
