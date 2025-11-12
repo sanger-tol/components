48451f1b83dab4e9c7aefe1d130a426e42dd54ef
Thomas Assheton <ta21@sanger.ac.uk>
2025-11-12 10:54:23 +0000

    Revert accidental Sandbox commit

--- components-ui/app/src/pages/Sandbox.tsx
+++ components-ui/app/src/pages/Sandbox.tsx
@@ -4,25 +4,6 @@ SPDX-FileCopyrightText: 2025 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-import { IFilter, ReadOnlyFilters, TsDataSource } from "../tol-ui/src";
-
 export function Sandbox() {
-  const filter: IFilter = {
-    and_: {
-      "first": {
-        "exists": {
-          negate: true,
-        }
-      },
-      "second": {
-        "eq": {
-          value: "test",
-        }
-      }
-    }
-  };
-  const objectType = "species";
-  const dataSource = new TsDataSource();
-
-  return <ReadOnlyFilters filter={filter} objectType={objectType} dataSource={dataSource} />;
+  return <></>;
 }
