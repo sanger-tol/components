c9103c7166472dc25ada435edf6624109d4e9173
Kiernan Harding <kh16@sanger.ac.uk>
2025-11-19 13:33:34 +0000

    TOLP-9213: New ObjectDetail Component

--- components-ui/app/src/tol-ui/src/general/ObjectDetail.tsx
+++ components-ui/app/src/tol-ui/src/general/ObjectDetail.tsx
@@ -4,20 +4,57 @@ SPDX-FileCopyrightText: 2023 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
+import { ReactNode } from "react";
+import {
+  UtilityBar,
+  TUtilityBarOrNull
+} from "..";
+
+
 export interface PObjectDetail {
-  data: object;
+  id: string;
+  data: Record<string, ReactNode>;
+  utilityBarConfig?: TUtilityBarOrNull;
+  contents?: ReactNode;
+  height?: any;
 }
 
 export function ObjectDetail(props: PObjectDetail) {
-  const { data } = props;
+  const {
+    id,
+    utilityBarConfig,
+    data,
+    contents,
+    height = "100%"
+  } = props;
+
+  const KeyValuePairs = Object.entries(data).map(([key, value]) => (
+    <p key={key}>
+      <strong>{key}:</strong> {value}
+    </p>
+  ));
+
 
   return (
-    <div className="tol-object-detail">
-      {Object.entries(data).map(([key, value]) => (
-        <p key={key}>
-          <strong>{key}:</strong> {value}
-        </p>
-      ))}
+    <div
+      className="tol-object-detail"
+      id={id}
+      style={{
+        height: height,
+      }}
+    >
+      {utilityBarConfig &&
+        <UtilityBar
+          id={id}
+          {...utilityBarConfig}
+        />
+      }
+      <div
+        className={utilityBarConfig ? "tol-component-contents-with-offset" : "tol-component-contents"}
+        style={{ overflow: 'auto' }}
+      >
+        {contents ? contents : KeyValuePairs}
+      </div>
     </div>
   );
 }
