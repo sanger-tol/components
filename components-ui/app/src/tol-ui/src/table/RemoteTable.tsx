2673711605ac117dc0e61d5d82f7bde3d46105e4
Thomas Assheton <ta21@sanger.ac.uk>
2025-11-19 09:27:29 +0000

    Remove debug log

--- components-ui/app/src/tol-ui/src/table/RemoteTable.tsx
+++ components-ui/app/src/tol-ui/src/table/RemoteTable.tsx
@@ -105,8 +105,6 @@ export function RemoteTable(props: PRemoteTable) {
     forceUpdate,
   } = props;
 
-  console.log(utilityBarConfig);
-
   // data and field information
   const [data, setData] = useState<any[]>([]);
   const [fieldMeta, setFieldMeta] = useState<FieldMeta>(
