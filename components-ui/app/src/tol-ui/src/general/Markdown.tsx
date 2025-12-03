0074680afe795a2bc74fdba5e3bdc3bed47ae558
Shweta Bhagat <sb83@mib117193s.internal.sanger.ac.uk>
2025-12-03 16:50:36 +0000

    TOLP-9335 Add code style guide to components

--- components-ui/app/src/tol-ui/src/general/Markdown.tsx
+++ components-ui/app/src/tol-ui/src/general/Markdown.tsx
@@ -5,12 +5,15 @@ SPDX-License-Identifier: MIT
 */
 
 import ReactMarkdown from "react-markdown";
+
+
 export interface PMarkdown {
   contents: string;
 }
 
 export function Markdown(props: PMarkdown) {
   const { contents } = props;
+
   return (
     // @ts-ignore
     <ReactMarkdown>
