5c2106d08b879e456e64fd747920785e890ec389
Kiernan Harding <kh16@sanger.ac.uk>
2026-01-07 16:35:53 +0000

    expand functionality

--- components-ui/app/src/tol-ui/src/constants/table.constants.ts
+++ components-ui/app/src/tol-ui/src/constants/table.constants.ts
@@ -4,6 +4,6 @@ SPDX-FileCopyrightText: 2026 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-export const DEFAULT_ROW_HEIGHT = 56;
-export const MAX_CELL_HEIGHT = 150;
+export const DEFAULT_ROW_HEIGHT = 60;
+export const COLLAPSED_ROW_MAX_HEIGHT = 150;
 export const CELL_PADDING = 24;
