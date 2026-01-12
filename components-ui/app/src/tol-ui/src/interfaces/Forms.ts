6ae9f49252b1450ca3510763a931e0139f8ab9a8
Luke Wilson <lw23@sanger.ac.uk>
2026-01-12 14:15:26 +0000

    type fix

--- components-ui/app/src/tol-ui/src/interfaces/Forms.ts
+++ components-ui/app/src/tol-ui/src/interfaces/Forms.ts
@@ -8,7 +8,7 @@ import React from "react";
 import { TMessageType, PButton, TsDataSource, PIcon } from "..";
 
 export interface IWaitingUpload {
-  message: string;
+  message: string | React.Node;
 }
 
 export interface IMessage {
