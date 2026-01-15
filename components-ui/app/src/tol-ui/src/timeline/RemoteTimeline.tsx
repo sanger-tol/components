c8409ac92d3580581aa6a479044adce868e38fc3
Thomas Assheton <ta21@sanger.ac.uk>
2026-01-15 10:32:13 +0000

    Documentation comment for RemoteTimeline

--- components-ui/app/src/tol-ui/src/timeline/RemoteTimeline.tsx
+++ components-ui/app/src/tol-ui/src/timeline/RemoteTimeline.tsx
@@ -23,6 +23,25 @@ interface IRemoteTimeline extends IRemoteTarget {
   titleDataPoint: string;
 }
 
+/**
+ * @autodoc
+ * 
+ * RemoteTimeline visualizes a timeline of events sourced from a remote `dataSource`.
+ * Utilizing the provided data, it creates a chronological representation of events,
+ * complete with titles, descriptions, and customizable icons.
+ * 
+ * @prop id - Unique identifier for the timeline instance, used in generating element IDs and for API interactions
+ * @prop objectType - The type of remote object being fetched, influencing the structure of the timeline based on API responses
+ * @prop dataSource - The data source utilized for API requests to retrieve timeline data
+ * @prop data - The data structure required to generate timeline items, containing titles, descriptions, and optional icons and colors for each event
+ * @prop dateWithDay - Optional boolean indicating whether dates should be displayed with the respective weekday
+ * @prop defaultIcon - Optional boolean to control whether a default icon should be displayed for timeline items that do not specify one
+ * @prop titleDataPoint - The key used to fetch the title for the timeline from the data object, providing context for the displayed events
+ * 
+ * @remarks
+ * This component handles various types of objects, automatically generating titles based on 
+ * the object type and its ID, making it flexible for different contexts.
+ */
 export function RemoteTimeline(props: IRemoteTimeline) {
   const {
     id,
