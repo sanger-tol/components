ec5ae33cac620149dacc2cd2be973bedc4363efc
Thomas Assheton <ta21@sanger.ac.uk>
2026-01-19 14:03:01 +0000

    Create TsDocParser class

---
+++ components-ui/app/src/tol-ui/src/autodoc/TsDocParser.tsx
@@ -0,0 +1,12 @@
+/*
+SPDX-FileCopyrightText: 2026 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+*/
+
+/**
+ * Parses TSDoc comments tagged with autodoc to extract component documentation
+ */
+export class TsDocParser {
+
+}
