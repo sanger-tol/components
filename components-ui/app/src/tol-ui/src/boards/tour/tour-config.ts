0a8d8285051a4c519f9390f8eb2b2df9dd3cd56e
Luke Wilson <lw23@sanger.ac.uk>
2026-01-19 14:27:19 +0000

    config added

---
+++ components-ui/app/src/tol-ui/src/boards/tour/tour-config.ts
@@ -0,0 +1,25 @@
+/*
+SPDX-FileCopyrightText: 2026 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+*/
+
+import { Tour } from "nextstepjs";
+
+const steps: Tour[] = [
+  {
+    tour: "initialTour",
+    steps: [
+      {
+        title: "1",
+        content: "2",
+        selector: "3",
+      },
+      {
+        title: "1",
+        content: "2",
+        selector: "3",
+      },
+    ],
+  },
+];
