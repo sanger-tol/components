8ef940b599e70072ef4fbedc673934b7ca7252f1
Thomas Assheton <ta21@sanger.ac.uk>
2026-02-09 14:55:42 +0000

    Add immediate attribute test

--- components-ui/app/src/test/services/DataObject.test.tsx
+++ components-ui/app/src/test/services/DataObject.test.tsx
@@ -8,6 +8,11 @@ import { describe, expect, test } from "vitest";
 import { mockDataObject } from "../mocks";
 
 describe("getFieldByName method", () => {
+  test("Ensures an attribute is fetched correctly", () => {
+    const obj = mockDataObject();
+    expect(obj.getFieldByName("name")).toBe("sampleName");
+  });
+
   test("Ensures a field's value is undefined if it does not exist", () => {
     const obj = mockDataObject();
     expect(obj.getFieldByName("doesNotExist.alsoDoesNotExist")).toBeUndefined();
