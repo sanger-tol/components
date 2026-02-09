152175c9b62a896c2c083ba5014b714cea01d5d7
Thomas Assheton <ta21@sanger.ac.uk>
2026-02-09 14:48:20 +0000

    Create test file

---
+++ components-ui/app/src/test/services/DataObject.test.tsx
@@ -0,0 +1,11 @@
+/*
+SPDX-FileCopyrightText: 2026 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+*/
+
+import { describe, expect, test } from "vitest";
+
+describe("getFieldByName method", () => {
+    
+});
