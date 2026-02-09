4e9d06cc55559a3362d1f7c2083da8e2c42e1b58
Thomas Assheton <ta21@sanger.ac.uk>
2026-02-09 11:22:33 +0000

    Port across getFieldByName

--- components-ui/app/src/tol-ui/src/datasource/DataObject.ts
+++ components-ui/app/src/tol-ui/src/datasource/DataObject.ts
@@ -8,24 +8,34 @@ export class DataObject {
   private _objectType: string;
   private _id: string;
   private _attributes: Map<string, any>;
-  private _relationships: Map<string, DataObject>;
+  private _relationships?: Map<string, DataObject>;
 
   constructor(
     objectType: string,
     id: string,
     attributes: Map<string, any>,
-    relationships: Map<string, DataObject> | undefined,
+    relationships?: Map<string, DataObject>,
   ) {
     this._objectType = objectType;
     this._id = id;
     this._attributes = attributes;
-    this._relationships = relationships || new Map();
+    this._relationships = relationships;
   }
 
   public get objectType(): string { return this._objectType; }
   public set objectType(type: string) { this._objectType = type; }
   public get id(): string { return this._id; }
   public set id(id: string) { this._id = id; }
-  // TODO NOT DOING IT FOR ATTRBIUTES; getFieldByName WILL BE IMPLEMENTED INSTEAD
-  public get relationships(): Map<string, DataObject> { return this._relationships }
+  public get relationships(): Map<string, DataObject> | undefined { return this._relationships }
+
+  public getFieldByName(field: string): any {
+    if (field.includes(".")) {
+      const [relationship, ...rest] = field.split(".");
+      const relationshipObject = this._relationships?.[relationship];
+      if (relationshipObject) {
+        return this.getFieldByName(rest.join("."));
+      }
+    }
+    return this._attributes[field];
+  }
 }
