6f0f6d8d7510ab0a80241d648a9e36b54ed75e8b
Thomas Assheton <ta21@sanger.ac.uk>
2026-02-09 11:09:06 +0000

    Port fields in IDataObject

--- components-ui/app/src/tol-ui/src/datasource/DataObject.ts
+++ components-ui/app/src/tol-ui/src/datasource/DataObject.ts
@@ -5,5 +5,21 @@ SPDX-License-Identifier: MIT
 */
 
 export class DataObject {
-  
+  private objectType: string;
+  private id: string;
+  private attributes: Map<string, any>;
+  private relationships: Map<string, DataObject>;
+
+  constructor(
+    objectType: string,
+    id: string,
+    attributes: Map<string, any>,
+    relationships: Map<string, DataObject> | undefined,
+  ) {
+    this.objectType = objectType;
+    this.id = id;
+    this.attributes = attributes;
+    this.relationships = new Map();
+    this.relationships = relationships || new Map();
+  }
 }
