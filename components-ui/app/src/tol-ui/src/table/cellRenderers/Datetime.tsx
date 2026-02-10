ba4447977ae78e7bfb73a9235ee534a632f7a19d
Kiernan Harding <kh16@sanger.ac.uk>
2026-02-10 22:32:39 +0000

    fix dates

--- components-ui/app/src/tol-ui/src/table/cellRenderers/Datetime.tsx
+++ components-ui/app/src/tol-ui/src/table/cellRenderers/Datetime.tsx
@@ -14,9 +14,7 @@ import {
 export function Datetime(props: PCell) {
   const { value } = props;
 
-  //const dateValue = Array.isArray(value) ? value[0] : value;
   const date = new Date(value!);
-  if (isNaN(date.getTime())) return null;
   
   const dateText = format(date, "dd/MM/yyyy");
   const dateContents = format(date, "dd/MM/yyyy HH:mm");
