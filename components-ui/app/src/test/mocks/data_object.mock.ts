4bbd35eb52a77cd4730a78c6d7e0769599bcd9d2
Logan Howat <lh31@sanger.ac.uk>
2026-02-19 14:27:12 +0000

    Pinning dependency to a version

--- components-ui/app/src/test/mocks/data_object.mock.ts
+++ components-ui/app/src/test/mocks/data_object.mock.ts
@@ -4,7 +4,7 @@ SPDX-FileCopyrightText: 2026 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-import { DataObject } from "../../tol-ui/src/datasource";
+import { DataObject } from "../../tol-ui/src";
 
 
 export const mockDataObject = () => new DataObject(
