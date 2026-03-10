34f9dffd78f82589c513cea972e154ad182c4c5a
Kiernan Harding <kh16@sanger.ac.uk>
2026-03-10 15:01:04 +0000

    sort image cellRenderers for now

--- components-ui/app/src/tol-ui/src/table/ImageModal.tsx
+++ components-ui/app/src/tol-ui/src/table/ImageModal.tsx
@@ -4,78 +4,35 @@ SPDX-FileCopyrightText: 2025 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-import { useState, useEffect } from "react";
-import { Icon, Modal } from "..";
+import { Modal } from "..";
+
 
 export interface PImageModal {
-  value: string | string[];
-  captions: string | string[];
+  value: string;
+  caption: string;
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  currentIndex: number;
 }
 
 export function ImageModal(props: PImageModal) {
-  const { value, captions, open, setOpen, currentIndex } = props;
-  const [modalIndex, setModalIndex] = useState<number>(currentIndex);
-
-  useEffect(() => {
-    if (Array.isArray(value)) {
-      const max = value.length - 1;
-      setModalIndex(Math.max(0, Math.min(currentIndex, max)));
-    } else {
-      setModalIndex(currentIndex);
-    }
-  }, [currentIndex, value]);
-
-  const multipleImages = Array.isArray(value);
-
-  const arrowPrev = (
-    setIndex: React.Dispatch<React.SetStateAction<number>>
-  ) => {
-    setIndex((prev) => (prev === 0 ? value.length - 1 : prev - 1));
-  };
-
-  const arrowNext = (
-    setIndex: React.Dispatch<React.SetStateAction<number>>
-  ) => {
-    setIndex((prev) => (prev === value.length - 1 ? 0 : prev + 1));
-  };
-
-  const Content = (
-    <div style={{ textAlign: "center" }}>
-      {multipleImages && (
-        <Icon
-          icon="caret-left"
-          onClick={() => arrowPrev(setModalIndex)}
-          size="2x"
-          className="tol-table-image-cell-arrow"
-        />
-      )}
-      <img
-        src={multipleImages ? value[modalIndex] : value}
-        className={'tol-table-image-modal-image'}
-        title={multipleImages ? value[modalIndex] : value}
-      />
-      {multipleImages && (
-        <Icon
-          icon="caret-right"
-          onClick={() => arrowNext(setModalIndex)}
-          size="2x"
-          className="tol-table-image-cell-arrow"
-        />
-      )}
-      <p className={"tol-table-image-modal-caption"}>
-        {multipleImages ? captions[modalIndex] : captions}
-      </p>
-    </div>
-  );
+  const { value, caption, open, setOpen } = props;
 
   return (
     <Modal
+      size="lg"
       open={open}
       setOpen={setOpen}
-      children={Content}
-    />
+    >
+      <div style={{ textAlign: "center" }}>
+        <img
+          src={value}
+          className="tol-table-image-modal-image"
+          title={value}
+        />
+        <p className="tol-table-image-modal-caption">
+          {caption}
+        </p>
+      </div>
+    </Modal>
   );
 }
