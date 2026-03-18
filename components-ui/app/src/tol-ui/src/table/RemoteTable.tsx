528e88c978f68a437f6c4876b4843a309418a41e
Luke Wilson <lw23@sanger.ac.uk>
2026-03-18 10:22:14 +0000

    requestedFields table fix

--- components-ui/app/src/tol-ui/src/table/RemoteTable.tsx
+++ components-ui/app/src/tol-ui/src/table/RemoteTable.tsx
@@ -383,6 +383,13 @@ export function RemoteTable(props: PRemoteTable) {
           });
       })
       .catch((error: any) => {
+        // Temp fix for 500 errors, due to empty requested fields
+        // TODO: Remove when the SDK handles empty requested fields better.
+        const errorMsg = error.response.data.errors[0].detail;
+        if (errorMsg.includes("in path")) {
+          setData([]);
+          return;
+        }
         setError(error.message);
         setData([]);
         console.error(error);
