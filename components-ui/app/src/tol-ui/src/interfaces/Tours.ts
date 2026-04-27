eaf65f3f0c4cb7c30a0732ca647588036fc9c8bc
Thomas Assheton <ta21@sanger.ac.uk>
2026-04-27 16:16:09 +0100

    Doc comment for ITourStep

--- components-ui/app/src/tol-ui/src/interfaces/Tours.ts
+++ components-ui/app/src/tol-ui/src/interfaces/Tours.ts
@@ -5,7 +5,16 @@ SPDX-License-Identifier: MIT
 */
 
 export interface ITourStep {
+  /**
+   * `testid` is used for Playwright tests. To not duplicate attributes, we re-use it for tours
+   */
   testid: string,
+  /**
+   * The title shown in the pop-up
+   */
   title: string,
+  /**
+   * The description shown in the pop-up
+   */
   description: string,
 };
