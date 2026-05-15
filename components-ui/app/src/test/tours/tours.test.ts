3da85f0ad49e57af1467eb60eb570b2f1174ce8a
Thomas Assheton <ta21@sanger.ac.uk>
2026-05-15 12:08:15 +0100

    Complete tests for hasTourBeenSeen

--- components-ui/app/src/test/tours/tours.test.ts
+++ components-ui/app/src/test/tours/tours.test.ts
@@ -6,7 +6,7 @@ SPDX-License-Identifier: MIT
 
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import { MockLocalStorage } from "../mocks/builtin";
-import { hasTourBeenSeen } from "../../tol-ui/src";
+import { hasTourBeenSeen, User } from "../../tol-ui/src";
 
 describe("hasTourBeenSeen", () => {
   let mockLocalStorage: MockLocalStorage;
@@ -16,7 +16,7 @@ describe("hasTourBeenSeen", () => {
     vi.stubGlobal("localStorage", mockLocalStorage);
   });
 
-  it("returns false when tour has not been seen and unauthenticated", async () => {
+  it("returns false when tour has not been seen and user unauthenticated", async () => {
     // User is `null` when unauthenticated
     const user = null;
     
@@ -24,12 +24,56 @@ describe("hasTourBeenSeen", () => {
     expect(result).toBe(false);
   });
 
-  it("returns true when tour has been seen and unauthenticated", async () => {
+  it("returns false when tour has not been been seen and user authenticated", async () => {
+    const user: User = {
+      id: "0",
+      name: "Name",
+      email: "name@example.com",
+      organisation: "Unit Test LTD",
+      roles: ["tol"],
+      tours_seen: {}
+    };
+
+    const result = await hasTourBeenSeen("tour", user);
+    expect(result).toBe(false);
+  });
+
+  it("returns false when tours_seen is NULL and user authenticated", async () => {
+    const user: User = {
+      id: "0",
+      name: "Name",
+      email: "name@example.com",
+      organisation: "Unit Test LTD",
+      roles: ["tol"],
+      tours_seen: null
+    };
+
+    const result = await hasTourBeenSeen("tour", user);
+    expect(result).toBe(false);
+  });
+
+  it("returns true when tour has been seen and user unauthenticated", async () => {
     // Add a seen tour
     mockLocalStorage.setItem("toursSeen", JSON.stringify({ "tour": true }))
 
     // User is `null` when unauthenticated
     const user = null;
+
+    const result = await hasTourBeenSeen("tour", user);
+    expect(result).toBe(true);
+  });
+
+  it("returns true when tour has been seen and user authenticated", async () => {
+    const user: User = {
+      id: "0",
+      name: "Name",
+      email: "name@example.com",
+      organisation: "Unit Test LTD",
+      roles: ["tol"],
+      tours_seen: {
+        "tour": true
+      }
+    };
 
     const result = await hasTourBeenSeen("tour", user);
     expect(result).toBe(true);
