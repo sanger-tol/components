f0e2c1a801577ffef4d130d2b0e16aa9946f88d4
Thomas Assheton <ta21@sanger.ac.uk>
2026-05-15 11:10:55 +0100

    Fully complete MockLocalStorage

--- components-ui/app/src/test/mocks/builtin/local-storage.mock.ts
+++ components-ui/app/src/test/mocks/builtin/local-storage.mock.ts
@@ -5,8 +5,20 @@ export class MockLocalStorage {
     this.data = {};
   }
 
+  clear() {
+    this.data = {};
+  }
+
   getItem(key: string): string | null {
     return this.data[key] || null;
+  }
+
+  get length(): number {
+    return Object.entries(this.data).length;
+  }
+
+  removeItem(key: string) {
+    delete this.data[key];
   }
 
   setItem(key: string, value: string) {
