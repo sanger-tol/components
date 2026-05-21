d95c415d022ecddd1fbf6ad353fa21ea0c7bc60b
Thomas Assheton <ta21@sanger.ac.uk>
2026-05-21 16:06:40 +0100

    Add doc comments for Priority

--- components-ui/app/src/tol-ui/src/table/cellRenderers/Priority.tsx
+++ components-ui/app/src/tol-ui/src/table/cellRenderers/Priority.tsx
@@ -7,13 +7,36 @@ SPDX-License-Identifier: MIT
 import { CellTooltip, Icon, PCellDisplay } from "../..";
 
 export interface PPriority extends PCellDisplay {
+  /**
+   * `true` if the condition for "highest" has been fulfilled
+   */
   highest?: boolean;
+  /**
+   * `true` if the condition for "high" has been fulfilled
+   */
   high?: boolean;
+  /**
+   * `true` if the condition for "medium" has been fulfilled
+   */
   medium?: boolean;
+  /**
+   * `true` if the condition for "low" has been fulfilled
+   */
   low?: boolean;
+  /**
+   * `true` if the condition for "lowest" has been fulfilled
+   */
   lowest?: boolean;
 }
 
+/**
+ * Renders an icon representing a priority.
+ * 
+ * There are 5 priority levels (in descending order): highest, high, medium, low, and lowest.
+ * The icon rendered depends on the which condition is met.
+ * While multiple conditions can be fulfilled at the same time,
+ * only the one representing the highest of the available priorities is rendered.
+ */
 export function Priority(props: PPriority) {
   const {
     value,
