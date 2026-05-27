10c59cae88062583ccd2e17f9d592b2f9a5ce81e
Thomas Assheton <ta21@sanger.ac.uk>
2026-05-27 10:16:14 +0100

    finish scaffolding first page

--- components-ui/app/src/tol-ui/src/table/cellRenderers/CellRendererModal.tsx
+++ components-ui/app/src/tol-ui/src/table/cellRenderers/CellRendererModal.tsx
@@ -1,6 +1,16 @@
 import { Dispatch, SetStateAction, useState } from "react";
 
-import { AttributeTitle, CellRendererParamOptions, FieldMeta, IconTooltip, IRemoteTarget, Modal } from "../..";
+import {
+  AttributeTitle,
+  Button,
+  BUTTONS,
+  CellRendererParamOptions,
+  FieldMeta,
+  IconTooltip,
+  IRemoteTarget,
+  Modal,
+  TCellRenderer
+} from "../..";
 
 export interface PCellRendererModal extends IRemoteTarget {
   open: boolean,
@@ -13,8 +23,10 @@ export interface PCellRendererModal extends IRemoteTarget {
 export function CellRendererModal(props: PCellRendererModal) {
   const { open, setOpen, objectType, dataSource, attributeId, fieldMeta, setFieldMeta } = props;
 
-  const [selectedParameter, setSelectedParameter] = useState<string | undefined>()
+  // const [renderer, setRenderer] = useState<TCellRenderer>();
+  const [selectedParameter, setSelectedParameter] = useState<string | undefined>();
 
+  // Used in the Header
   const TooltipHelp = (
     <ul>
       <li>
@@ -26,6 +38,7 @@ export function CellRendererModal(props: PCellRendererModal) {
     </ul>
   );
 
+  // The header is shared between both pages
   const Header = (
     <>
       <h5>
@@ -45,15 +58,39 @@ export function CellRendererModal(props: PCellRendererModal) {
     </>
   );
 
+  // The dropdown where the cell renderer type is chosen. This determines which parameters
+  // need to be shown in ParameterList
   const CellRendererSelector = <></>;
 
+  // Each parameter associated with the selected cell renderer type
+  const ParameterList = (
+    <>
+      <p>Parameter list here</p>
+    </>
+  );
+
+  // Shown only on the first page. Needs to be added as the action button instead of at the
+  // bottom of the page so that it sits alongside the close button.
+  // (In contrast to the second page, which has custom buttons at the bottom)
+  const AddCellRendererButton = (
+    <Button
+      {...BUTTONS.ADD}
+      // disabled={!rendererHasPendingChanges || requiredParamsCount > filledParamsCount}
+      // onClick={onAddNewRenderer}
+    />
+  );
+
+  // The first page of the modal where the cell renderer type is selected and its parameters
+  // shown. Some parameters can be edited directly, while others take you to the second page
+  // to edit them
   const FirstPage = (
     <>
       {CellRendererSelector}
       <hr />
-      <p>Params</p>
+      {ParameterList}
     </>
   );
+  // The second page of the modal: a dedicated space to edit a specific parameter
   const SecondPage = <CellRendererParamOptions param={selectedParameter || ""} />;
 
   return (
@@ -62,6 +99,8 @@ export function CellRendererModal(props: PCellRendererModal) {
       setOpen={setOpen}
       header={Header}
       size={selectedParameter ? "sm" : "xs"}
+      closeButton={!selectedParameter}
+      actionButton={selectedParameter ? undefined : AddCellRendererButton}
     >
       {selectedParameter ? SecondPage : FirstPage}
     </Modal>
