e0f95ffa13cc8f46e1def4eb40c95c68e2afd401
Logan Howat <lh31@sanger.ac.uk>
2026-06-01 15:45:55 +0100

    Adding new Playwright test for testing users permissions on others Boards

---
+++ components-ui/app/src/test/playwright/tests/board/boardPermissions.spec.ts
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: 2025 Genome Research Ltd.
+//
+// SPDX-License-Identifier: MIT
+
+import { expect, test } from '@playwright/test';
+import {
+  setAuth,
+  addUserToDB,
+  createBoardForUser,
+  setBoard,
+  createBoardID,
+} from '../helpers';
+
+const headless = !!(process.env.CI || process.env.HEADLESS);
+
+test.use({ headless: headless });
+
+test('Cannot edit others boards', async ({ page }) => {
+  // Sets a user session up for the browser
+  const browserUserBoard = createBoardID();
+  await setAuth({ page });
+  await setBoard({ page, boardID: browserUserBoard });
+  await expect(page.getByTestId("board-enter-edit-mode-button")).toBeVisible();
+
+  // Add a new user and give them a board in the DB
+  // This is not the same user as our browser session
+  const { userID } = await addUserToDB();
+  const otherUserBoard = await createBoardForUser(String(userID));
+  await page.goto(`/board/${otherUserBoard}`);
+  await expect(page.getByTestId("board-enter-edit-mode-button")).not.toBeVisible();
+});
