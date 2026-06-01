58da1a7bfd1906b6ba5881e6b2904357235e7f2a
Logan Howat <lh31@sanger.ac.uk>
2026-06-01 10:57:43 +0100

    TOLP-9913: Adding generic Playwright changes

---
+++ components-ui/app/src/test/mocks/builtin/local-storage.mock.ts
@@ -0,0 +1,33 @@
+/*
+SPDX-FileCopyrightText: 2026 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+*/
+
+export class MockLocalStorage {
+  private data: object;
+  
+  constructor() {
+    this.data = {};
+  }
+
+  clear() {
+    this.data = {};
+  }
+
+  getItem(key: string): string | null {
+    return this.data[key] || null;
+  }
+
+  get length(): number {
+    return Object.entries(this.data).length;
+  }
+
+  removeItem(key: string) {
+    delete this.data[key];
+  }
+
+  setItem(key: string, value: string) {
+    this.data[key] = value;
+  }
+}
