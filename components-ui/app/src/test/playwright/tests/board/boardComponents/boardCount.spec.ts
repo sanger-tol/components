ed3f4061c2d386cd09a4be7db3e993bda0e6bddf
Thomas Assheton <ta21@sanger.ac.uk>
2026-06-09 16:11:19 +0100

    Lint boardCount

--- components-ui/app/src/test/playwright/tests/board/boardComponents/boardCount.spec.ts
+++ components-ui/app/src/test/playwright/tests/board/boardComponents/boardCount.spec.ts
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 import {
   addComponent,
   setBoard,
@@ -13,7 +13,7 @@ import {
   enterEditMode,
   exitEditMode,
   createBoardId
-} from '../../helpers'
+} from "../../helpers";
 
 const headless = !!(process.env.CI || process.env.HEADLESS);
 const BOARD_ID = createBoardId();
@@ -23,16 +23,16 @@ test.use({ headless: headless });
 test.beforeEach(async ({ page }) => {
   await setAuth(page);
   await setBoard({ page, boardID: BOARD_ID });
-  await enterEditMode( page );
+  await enterEditMode(page);
 });
 
 test.afterEach(async ({ page }) => {
-  await exitEditMode( page );
+  await exitEditMode(page);
 });
 
 const filterCountComponent = async (page) => {
   // get the count before filtering
-  const countBefore = await page.locator('.tol-count').textContent();
+  const countBefore = await page.locator(".tol-count").textContent();
 
   await addComponentFilter(
     page,
@@ -46,15 +46,15 @@ const filterCountComponent = async (page) => {
   // check the count has changed
   // TODO This sleep should be removed at some point
   await sleep(200);
-  const countAfter = await page.locator('.tol-count').textContent();
+  const countAfter = await page.locator(".tol-count").textContent();
   expect(countAfter).not.toBe(countBefore);
 }
 
-test('manage dashboard', async ({ page }) => {
+test("manage dashboard", async ({ page }) => {
   await addComponent(page, 0, "statistics", "Small");
 
   await filterCountComponent(page);
 
   await deleteComponent(page, "statistics", 0);
-  await expect(page.locator('.tol-count')).not.toBeVisible();
+  await expect(page.locator(".tol-count")).not.toBeVisible();
 });
