863a7abf8bb4b7af816782d7eaab276db21aecde
Thomas Assheton <ta21@sanger.ac.uk>
2026-06-09 16:20:48 +0100

    Document edit mode helpers

--- components-ui/app/src/test/playwright/tests/helpers/editMode.ts
+++ components-ui/app/src/test/playwright/tests/helpers/editMode.ts
@@ -4,13 +4,23 @@
 
 import { Page } from "@playwright/test";
 
+/**
+ * Enters Edit Mode on a board (when not already in Edit mode)
+ * @param page The Playwright page handle
+ */
 export const enterEditMode = async (page: Page) => {
   // click the enter edit mode button
   await page.getByTestId("board-enter-edit-mode-button").click();
 };
 
+/**
+ * Exits Edit Mode on a board (when already in Edit Mode)
+ * @param page The Playwright page handle
+ */
 export const exitEditMode = async (page: Page) => {
   // click the exit edit mode button
   await page.getByTestId("board-exit-edit-mode-button").click();
+  
+  // This might force the view down a bit, so scroll back to the top
   await page.evaluate(() => window.scrollTo(0, 0));
 };
