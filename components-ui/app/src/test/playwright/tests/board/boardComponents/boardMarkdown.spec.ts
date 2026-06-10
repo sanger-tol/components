1cd54f3c999469b214a0124b113db9dc9e7e5c69
Thomas Assheton <ta21@sanger.ac.uk>
2026-06-10 10:26:13 +0100

    Refine boardMarkdown

--- components-ui/app/src/test/playwright/tests/board/boardComponents/boardMarkdown.spec.ts
+++ components-ui/app/src/test/playwright/tests/board/boardComponents/boardMarkdown.spec.ts
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { expect, test } from "@playwright/test";
+import { expect, Page, test } from "@playwright/test";
 import {
   addComponent,
   setBoard,
@@ -30,30 +30,36 @@ test.afterEach(async ({ page }) => {
   await exitEditMode(page);
 });
 
-const editMarkDownComponentOnAdd = async ({ page }) => {
+/**
+ * Adds text to an empty Text component
+ * @param page The Playwright page handle
+ * @param componentIndex Out of all the Text components on the screen, which is it? Zero-indexed
+ */
+const addTextToMarkdownComponent = async (page: Page, componentIndex: number) => {
   // get the markdown editor textarea
-  const mardownEditor = page.locator(".tol-markdown-viewer textarea");
+  const markdownEditor = page.locator(".tol-markdown-viewer textarea");
 
   // click into the markdown editor and type text to simulate real user input
-  await mardownEditor.click();
+  await markdownEditor.click();
   await page.keyboard.type("Test Text", { delay: 10 });
   await sleep(1000);
-  await expect(mardownEditor).toHaveValue("Test Text");
+  await expect(markdownEditor).toHaveValue("Test Text");
 
   // click the preview button
-  await clickUtilityBarButton(page, "preview-markdown", 0);
+  await clickUtilityBarButton(page, "preview-markdown", componentIndex);
 
-  // Count is 3, once for preview, editor and saved view (even though only two are visible)
-  await expect(page.getByText("Test Text")).toHaveCount(3);
-}
+  // Check the text is in both of the expected places
+  await expect(page.locator("[data-testid=\"board-component-text\"] textarea")).toHaveText("Test Text");
+  await expect(page.locator("[data-testid=\"board-component-text\"] p")).toHaveText("Test Text");
+};
 
 const saveMarkDownComponent = async ({ page }) => {
   await clickUtilityBarButton(page, "save-markdown", 0);
 }
 
 test("manage dashboard", async ({ page }) => {
-  await addComponent( page, 0, "text", "Small");
-  await editMarkDownComponentOnAdd({ page });
+  await addComponent(page, 0, "text", "Small");
+  await addTextToMarkdownComponent(page, 0);
   await saveMarkDownComponent({ page });
   await deleteComponent(page, "text", 0);
   await expect(page.locator(".tol-markdown-viewer")).not.toBeVisible();
