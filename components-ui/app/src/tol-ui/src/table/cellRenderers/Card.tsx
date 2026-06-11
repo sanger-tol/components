16ba2015825a34870c2295dd9386518e5b435590
Thomas Assheton <ta21@sanger.ac.uk>
2026-06-11 12:09:42 +0100

    Document Card

--- components-ui/app/src/tol-ui/src/table/cellRenderers/Card.tsx
+++ components-ui/app/src/tol-ui/src/table/cellRenderers/Card.tsx
@@ -8,12 +8,29 @@ import { COLOURS, Markdown, Tag } from "../..";
 import type { PCellDisplay } from "../..";
 
 export interface PCard extends PCellDisplay {
+  /**
+   * Markdown Parameter: The unformatted markdown to display in the cell.
+   * By this stage, inserted attribute data (from the '${}' syntax) has already been added.
+   */
   content: string;
+  /**
+   * Condition Parameter: Whether the success background colour should be used
+   */
   successBackground: boolean;
+  /**
+   * Condition Parameter: Whether the warning background colour should be used
+   */
   warningBackground: boolean;
+  /**
+   * Condition Parameter: WHether the error background colour should be used
+   */
   errorBackground: boolean;
 }
 
+/**
+ * Cell renderer that displays markdown content in a rectangular container,
+ * with a background indicating the status of the content.
+ */
 export function Card(props: PCard) {
   const {
     content,
