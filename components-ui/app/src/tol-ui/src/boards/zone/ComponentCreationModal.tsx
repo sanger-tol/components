14a11e95aa39da0b6f89f00d47be5d156281472c
Kiernan Harding <kh16@sanger.ac.uk>
2026-06-12 08:10:19 +0100

    sort datasource out

--- components-ui/app/src/tol-ui/src/boards/zone/ComponentCreationModal.tsx
+++ components-ui/app/src/tol-ui/src/boards/zone/ComponentCreationModal.tsx
@@ -64,11 +64,11 @@ export function ComponentCreationModal(props: PComponentCreationModal) {
     )
       .then((res) => {
         const component = res.data;
-        const z = defineBoardEntityInParent<IComponent, IZone>(
+        const z = defineBoardEntityInParent(
           BOARD_ENTITIES.ENTITIES.COMPONENT,
           component,
           zone
-        )
+        ) as IZone;
         setZone({ ...z });
         reset();
       })
