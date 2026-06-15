c92a0dc68e6d589ce72ab7969dd18f6b74350528
Thomas Assheton <ta21@sanger.ac.uk>
2026-06-15 15:36:12 +0100

    TOLP-9936 Curations changes

--- components-ui/app/src/tol-ui/src/forms/SingleSelect.tsx
+++ components-ui/app/src/tol-ui/src/forms/SingleSelect.tsx
@@ -6,19 +6,56 @@ SPDX-License-Identifier: MIT
 
 import { useEffect, useState } from "react";
 import { SelectPicker } from "rsuite";
+
 import { TLabelAndValueData } from "..";
 
-
 export interface PSingleSelect {
+  /**
+   * The options to display in the dropdown
+   */
   data: string[] | TLabelAndValueData;
+  /**
+   * The text to display when no option has been chosen
+   */
   placeholder?: string;
+  /**
+   * The option currently selected
+   */
   value: string;
+  /**
+   * Runs when the user selects an option, giving its value
+   */
   onChange: (value: string) => void;
+  /**
+   * Whether to display this component as a block (fill the width)
+   */
   block?: boolean;
+  /**
+   * Whether to disable this component
+   */
   disabled?: boolean;
+  /**
+   * Whether the component is in a loading state
+   * (and so shouldn't be interacted with yet)
+   */
   loading?: boolean;
+  /**
+   * Test ID to use for Playwright tests
+   */
   testid?: string;
+  /**
+   * The default option to be selected
+   */
   defaultValue?: string;
+  /**
+   * Whether to show the 'x' button that clears the selection option
+   * such that nothing is selected
+   */
+  cleanable?: boolean;
+  /**
+   * Whether to display the search bar
+   */
+  searchable?: boolean;
 }
 
 export const SingleSelect = (props: PSingleSelect) => {
