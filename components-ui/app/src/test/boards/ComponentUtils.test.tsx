8ad7e24646447a2a16d5522e4884a4ad8bdcb3f0
Thomas Assheton <ta21@sanger.ac.uk>
2026-06-16 11:24:30 +0100

    Add getWidgetOrder test suite

---
+++ components-ui/app/src/test/boards/ComponentUtils.test.tsx
@@ -0,0 +1,43 @@
+/*
+SPDX-FileCopyrightText: 2026 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+*/
+
+import { describe, expect, test } from "vitest";
+import type { Layout } from "react-grid-layout";
+import { getWidgetOrder } from "../../tol-ui/src";
+
+describe("getWidgetOrder function", () => {
+  test("Components are sorted by y position", () => {
+    const layout: Layout[] = [
+      {
+        i: "c_lygdf8dgo8",
+        x: 0,
+        y: 80,
+        w: 100,
+        h: 100,
+      },
+      {
+        i: "c_LKJHG87ssS",
+        x: 0,
+        y: 20,
+        w: 100,
+        h: 100,
+      },
+      {
+        i: "c_LKJh87tdGd",
+        x: 0,
+        y: 300,
+        w: 100,
+        h: 100,
+      },
+    ];
+
+    expect(getWidgetOrder(layout)).toEqual([
+      "c_LKJHG87ssS",
+      "c_lygdf8dgo8",
+      "c_LKJh87tdGd",
+    ]);
+  });
+});
