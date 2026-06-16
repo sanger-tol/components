f9228249a5605083d26347da6431deeea9fd622a
Thomas Assheton <ta21@sanger.ac.uk>
2026-06-16 11:26:58 +0100

    Condense tests

--- components-ui/app/src/test/boards/ComponentUtils.test.tsx
+++ components-ui/app/src/test/boards/ComponentUtils.test.tsx
@@ -13,31 +13,47 @@ describe("getWidgetOrder function", () => {
     const layout: Layout[] = [
       {
         i: "c_lygdf8dgo8",
-        x: 0,
-        y: 80,
-        w: 100,
-        h: 100,
+        x: 0, y: 80,
+        w: 100, h: 100,
       },
       {
         i: "c_LKJHG87ssS",
-        x: 0,
-        y: 20,
-        w: 100,
-        h: 100,
+        x: 0, y: 20,
+        w: 100, h: 100,
       },
       {
         i: "c_LKJh87tdGd",
-        x: 0,
-        y: 300,
-        w: 100,
-        h: 100,
+        x: 0, y: 300,
+        w: 100, h: 100,
       },
     ];
 
     expect(getWidgetOrder(layout)).toEqual([
-      "c_LKJHG87ssS",
-      "c_lygdf8dgo8",
-      "c_LKJh87tdGd",
+      "c_LKJHG87ssS", "c_lygdf8dgo8", "c_LKJh87tdGd",
+    ]);
+  });
+
+  test("The x position is used as a secondary sort", () => {
+    const layout: Layout[] = [
+      {
+        i: "c_lygdf8dgo8",
+        x: 0, y: 80,
+        w: 100, h: 100,
+      },
+      {
+        i: "c_LKJHG87ssS",
+        x: 40, y: 20,
+        w: 100, h: 100,
+      },
+      {
+        i: "c_LKJh87tdGd",
+        x: 0, y: 20,
+        w: 100, h: 100,
+      },
+    ];
+
+    expect(getWidgetOrder(layout)).toEqual([
+      "c_LKJh87tdGd", "c_LKJHG87ssS", "c_lygdf8dgo8",
     ]);
   });
 });
