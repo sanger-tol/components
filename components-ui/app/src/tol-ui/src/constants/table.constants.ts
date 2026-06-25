aa59b50cb9201e9b9999ebd61605e42a7971f9b9
Thomas Assheton <ta21@sanger.ac.uk>
2026-06-25 15:19:07 +0100

    Table sizing adjustments

--- components-ui/app/src/tol-ui/src/constants/table.constants.ts
+++ components-ui/app/src/tol-ui/src/constants/table.constants.ts
@@ -10,7 +10,7 @@ export const COLLAPSED_ROW_MAX_HEIGHT = 150;
 export const CELL_PADDING = 24;
 
 export const DEFAULT_COLUMN_WIDTH = 200;
-export const MIN_COLUMN_WIDTH = 120;
+export const MIN_COLUMN_WIDTH = 100;
 export const MAX_COLUMN_WIDTH = 500;
 export const ROW_TOOLS_COLUMN_MAX_WIDTH = 50;
 export const ROW_TOOLS_COLUMN_SINGLE_ITEM_WIDTH = 34;
