514e95b7ebfcc959cfff058e3a988a1abea2e071
Logan Howat <lh31@sanger.ac.uk>
2026-07-13 13:01:37 +0100

    Fixing zone re-ordering

--- components-ui/app/src/test/playwright/tests/board/boardReOrdering.spec.ts
+++ components-ui/app/src/test/playwright/tests/board/boardReOrdering.spec.ts
@@ -92,11 +92,10 @@ test("Can re-order zones", async ({ page }) => {
   const zones = page.getByTestId("zone-container");
   // This locator filters for only zone editable titles, still using a data-test-id
   const zoneTitles = page.locator('[data-testid="zone-utility-bar"] .tol-editable-title');
-  const zone1 = zones.filter({ has: page.getByText("Zone 1", { exact: true }) }).first();
 
   await expect(zoneTitles).toHaveText(["Zone 1", "Zone 2"]);
 
-  await zone1.getByTestId("move-zone-down-button").click();
+  await page.getByTestId("move-zone-down-button").first().click();
   await page.getByText("Save Layouts").click();
 
   await expect(zoneTitles).toHaveText(["Zone 2", "Zone 1"]);
