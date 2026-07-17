16d84f4705b779fb90f0f36110004096161396cf
Thomas Assheton <ta21@sanger.ac.uk>
2026-07-17 14:58:50 +0100

    Set up boardPerformance test

--- components-ui/app/src/test/playwright/tests/board/boardPerformance.spec.ts
+++ components-ui/app/src/test/playwright/tests/board/boardPerformance.spec.ts
@@ -4,14 +4,39 @@
 
 import { test } from "@playwright/test";
 
-import { addComponent, clickUtilityBarButton, enterEditMode } from "../helpers";
+import {
+  addComponent,
+  configureTable,
+  createBoardId,
+  enterEditMode,
+  exitEditMode,
+  isInHeadlessMode,
+  setAuth,
+  setBoard
+} from "../helpers";
+
+let BOARD_ID: string;
+
+test.use({ headless: isInHeadlessMode });
+
+test.beforeEach(async ({ page }) => {
+  BOARD_ID = createBoardId();
+  await setAuth(page);
+  await setBoard(page, BOARD_ID);
+  await enterEditMode(page);
+});
+
+test.afterEach(async ({ page }) => {
+  await exitEditMode(page);
+});
 
 test("big table", async ({ page }) => {
-  await enterEditMode(page);
   await addComponent(page, 0, "table");
-  await clickUtilityBarButton(
-    page,
-    page.getByTestId("board-component-table"),
-    "table-config-button"
-  );
+  // await configureTable(
+  //   page,
+  //   page.getByTestId("board-component-table"),
+  //   {
+  //     activeColumns: ["Species Name", "Priority"]
+  //   }
+  // )
 });
