84500e69bc6e830dd024b857e16e38d3ca2c3794
Kiernan Harding <kh16@sanger.ac.uk>
2026-07-20 11:43:41 +0100

    update buttons etc

--- components-ui/app/src/tol-ui/src/config/buttons.config.ts
+++ components-ui/app/src/tol-ui/src/config/buttons.config.ts
@@ -27,6 +27,13 @@ export const BUTTONS: Record<string, PButton> = {
     tooltip: "Cancel",
     position: "right",
   },
+  REMOVE: {
+    outline: true,
+    type: "error",
+    icon: "minus",
+    tooltip: "Remove",
+    position: "right",
+  },
   CLOSE: {
     type: "error",
     icon: "xmark",
