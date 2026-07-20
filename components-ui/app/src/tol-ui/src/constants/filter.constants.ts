bcd05c728843072b7d9fb431f454c8da60dbd205
Kiernan Harding <kh16@sanger.ac.uk>
2026-07-20 10:47:26 +0100

    fix saving filters

--- components-ui/app/src/tol-ui/src/constants/filter.constants.ts
+++ components-ui/app/src/tol-ui/src/constants/filter.constants.ts
@@ -7,3 +7,5 @@ SPDX-License-Identifier: MIT
 export const NO_FILTERS_APPLIED = "No filters applied, click here to add...";
 export const FILTER_ALREADY_EXISTS =
     "A filter already exists in the filtering system. Please remove it before adding this filter.";
+
+export const FILTER_INPUT_DELAY = 800;
