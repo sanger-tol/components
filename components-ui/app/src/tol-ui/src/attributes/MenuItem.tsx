85c237f14fd9ee9e54c0d080364d13af171a6e01
Thomas Assheton <ta21@sanger.ac.uk>
2026-07-27 15:08:56 +0100

    Add collapsible functionality to AttributeSelector for provenance

--- components-ui/app/src/tol-ui/src/attributes/MenuItem.tsx
+++ components-ui/app/src/tol-ui/src/attributes/MenuItem.tsx
@@ -5,12 +5,18 @@ SPDX-License-Identifier: MIT
 */
 
 
+import { forwardRef, useState } from "react";
+import type { HTMLAttributes } from "react";
+import { Animation as RSAnimation } from "rsuite";
+
 import {
   TsDataSource,
   Icon,
   SourceTag,
   truncateString,
-  AttributeTitle
+  AttributeTitle,
+  Button,
+  Col,
 } from "..";
 
 export interface PMenuItem {
@@ -27,14 +33,16 @@ export interface PMenuItem {
 export function MenuItem(props: PMenuItem) {
   const { source, field, authoritative, disabledValues,
     objectType, dataSource, displaySource } = props;
+  
+  const [provenancePickerOpen, setProvenancePickerOpen] = useState(false);
 
   const disabled =
     disabledValues && Object.keys(disabledValues).includes(field);
 
   const lettersToDisplay = window.innerWidth < 576 ? 30 : 60;
 
-  return (
-    <div key={field} className="tol-attribute-selector-menu-item-container">
+  const ItemContents = (
+    <>
       <div className="tol-attribute-selector-menu-item-inner-container">
         <div className="tol-attribute-selector-display-name">
           <AttributeTitle
@@ -50,6 +58,33 @@ export function MenuItem(props: PMenuItem) {
         </div>
       </div>
       {displaySource && source && <SourceTag source={source} />}
+    </>
+  );
+
+  const ProvenancePicker = forwardRef<
+    HTMLParagraphElement, HTMLAttributes<HTMLParagraphElement>
+  >((props, ref) => (
+    <div {...props} ref={ref}>
+      <p>adf</p>
     </div>
+  ));
+
+  return (
+    <Col className="tol-attribute-selector-menu-item">
+      <div key={field} className="tol-attribute-selector-menu-item-container">
+        {ItemContents}
+        {true && ( // TODO change to some indicator of provenance being active
+          <Button
+            outline
+            icon={provenancePickerOpen ? "angle-up" : "angle-down"}
+            tooltip="Configure Provenance"
+            onClick={() => setProvenancePickerOpen(prev => !prev)}
+          />
+        )}
+      </div>
+      <RSAnimation.Collapse in={provenancePickerOpen}>
+        {(props, ref) => <ProvenancePicker {...props} ref={ref} />}
+      </RSAnimation.Collapse>
+    </Col>
   );
 }
