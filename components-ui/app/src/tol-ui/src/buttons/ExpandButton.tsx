af47b0e7ff91065f1dff6d435d83d3b23e3dc1d3
Thomas Assheton <ta21@sanger.ac.uk>
2026-07-31 14:49:44 +0100

    Add doc comments

--- components-ui/app/src/tol-ui/src/buttons/ExpandButton.tsx
+++ components-ui/app/src/tol-ui/src/buttons/ExpandButton.tsx
@@ -8,10 +8,19 @@ import { Button } from "..";
 import type { PButton } from "..";
 
 export interface PExpandButton extends PButton {
+  /**
+   * Whether the content this button controls is expanded
+   */
   expanded: boolean;
+  /**
+   * Callback used to update the expanded state
+   */
   setExpanded: (expanded: boolean) => void;
 }
 
+/**
+ * A toggle button that switches between expanded and collapsed states
+ */
 export function ExpandButton(props: PExpandButton) {
   const { expanded, setExpanded, ...buttonProps } = props;
   const propsToPassOn = {
