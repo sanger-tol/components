b392130d54636f2f563976d0c9cad053a2c1cbfe
Luke Wilson <lw23@sanger.ac.uk>
2026-08-05 14:51:56 +0100

    revert user profile

--- components-ui/app/src/tol-ui/src/forms/defined-forms/UserProfile.tsx
+++ components-ui/app/src/tol-ui/src/forms/defined-forms/UserProfile.tsx
@@ -127,42 +127,7 @@ export function UserProfile(props: PUserProfile) {
       component: isLoading ? (
         <TolLoader content="Loading profile..." vertical size="md" />
       ) : (
-        // ProfileForm
-        <RemoteBreadcrumbNav
-          size="lg"
-          separator=">"
-          // dataSource={LOCAL_DS}
-          dataSource={
-            new TsDataSource({
-              url: "https://portal.tol.sanger.ac.uk",
-              apiPath: "/api/v1",
-              apiDataPath: "/data",
-              dataspace: "tol_production",
-            })
-          }
-          objectType={"species"}
-          lastLinkable
-          // links={[
-          //   { text: "Chordata", url: "/explore?phylum=Chordata" },
-          //   { text: "Actinopterygii", url: "/explore?class=Actinopterygii" },
-          //   {
-          //     text: "Pleuronectiformes",
-          //     url: "/explore?order=Pleuronectiformes",
-          //   },
-          //   { text: "Soleidae", url: "/explore?family=Soleidae" },
-          // ]}
-          attributes={[
-            "goat_kingdom_name",
-            "goat_class_name",
-            "goat_order_name",
-            "goat_family_name",
-            "goat_scientific_name",
-          ]}
-          urlConstructFn={(attribute, value) =>
-            `/explore?${attribute}=${value}`
-          }
-          objectId={"442079"}
-        />
+        ProfileForm
       ),
     },
   ];
