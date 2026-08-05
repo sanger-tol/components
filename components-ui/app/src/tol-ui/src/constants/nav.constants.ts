e6a9c7f38093627a8be74092f4d18403e1d5499f
Luke Wilson <lw23@sanger.ac.uk>
2026-08-05 13:47:55 +0100

    revert profile nav link

--- components-ui/app/src/tol-ui/src/constants/nav.constants.ts
+++ components-ui/app/src/tol-ui/src/constants/nav.constants.ts
@@ -24,5 +24,5 @@ export const URL_PATHS: { [key: string]: string } = {
   PAGE_NOT_FOUND: "/page-not-found",
   BOARD: "/board",
   MY_BOARDS: "/my-boards",
-  PROFILE: "/user/a/b/c/d/e/f/profile",
+  PROFILE: "/profile",
 } as const;
