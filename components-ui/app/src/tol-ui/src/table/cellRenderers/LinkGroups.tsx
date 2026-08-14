c0658166e4f2858a9ba7ae312ff53d65ea40a024
Thomas Assheton <ta21@sanger.ac.uk>
2026-08-14 12:10:22 +0100

    Create new cell renderer

---
+++ components-ui/app/src/tol-ui/src/table/cellRenderers/LinkGroups.tsx
@@ -0,0 +1,13 @@
+/*
+SPDX-FileCopyrightText: 2026 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+*/
+
+export interface PLinkGroups {
+
+}
+
+export default function LinkGroups(props: PLinkGroups) {
+  return <></>
+}
