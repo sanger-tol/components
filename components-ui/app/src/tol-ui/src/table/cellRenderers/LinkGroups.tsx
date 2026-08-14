31b61046050f9a8f6a23d50369b289deb843539b
Thomas Assheton <ta21@sanger.ac.uk>
2026-08-14 13:17:15 +0100

    Get a basic version of the renderer working

--- components-ui/app/src/tol-ui/src/table/cellRenderers/LinkGroups.tsx
+++ components-ui/app/src/tol-ui/src/table/cellRenderers/LinkGroups.tsx
@@ -5,9 +5,49 @@ SPDX-License-Identifier: MIT
 */
 
 export interface PLinkGroups {
-
+  firstGroupTitle?: string;
+  firstGroupFirstLinkUrl?: string;
+  firstGroupFirstLinkText?: string;
+  firstGroupSecondLinkUrl?: string;
+  firstGroupSecondLinkText?: string;
+  firstGroupThirdLinkUrl?: string;
+  firstGroupThirdLinkText?: string;
+  firstGroupFourthLinkUrl?: string;
+  firstGroupFourthLinkText?: string;
+  secondGroupTitle?: string;
+  secondGroupFirstLinkUrl?: string;
+  secondGroupFirstLinkText?: string;
+  secondGroupSecondLinkUrl?: string;
+  secondGroupSecondLinkText?: string;
+  secondGroupThirdLinkUrl?: string;
+  secondGroupThirdLinkText?: string;
+  secondGroupFourthLinkUrl?: string;
+  secondGroupFourthLinkText?: string;
+  thirdGroupTitle?: string;
+  thirdGroupFirstLinkUrl?: string;
+  thirdGroupFirstLinkText?: string;
+  thirdGroupSecondLinkUrl?: string;
+  thirdGroupSecondLinkText?: string;
+  thirdGroupThirdLinkUrl?: string;
+  thirdGroupThirdLinkText?: string;
+  thirdGroupFourthLinkUrl?: string;
+  thirdGroupFourthLinkText?: string;
 }
 
-export default function LinkGroups(props: PLinkGroups) {
-  return <></>
+export function LinkGroups(props: PLinkGroups) {
+  return (
+    <>
+      {["first", "second", "third"].map(group =>
+        <div key={group}>
+          {/* TODO VALIDATION. IF NO LINK FOR THIS SKIP */}
+          <h6>{props[`${group}GroupTitle`]}</h6>
+          {["First", "Second", "Third", "Fourth"].map(link =>
+            <a key={link} href={props[`${group}Group${link}LinkUrl`]}>{props[`${group}Group${link}LinkText`]}</a>
+          )}
+        </div>
+      )}
+    </>
+  )
+  
+  return 
 }
