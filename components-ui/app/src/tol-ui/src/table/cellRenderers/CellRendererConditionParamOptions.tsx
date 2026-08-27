80c2c73799c4bb6e4d916b77219a20dc2accf025
Thomas Assheton <ta21@sanger.ac.uk>
2026-08-27 13:56:39 +0100

    Refactor condition configurer to use new condition format

--- components-ui/app/src/tol-ui/src/table/cellRenderers/CellRendererConditionParamOptions.tsx
+++ components-ui/app/src/tol-ui/src/table/cellRenderers/CellRendererConditionParamOptions.tsx
@@ -101,51 +101,111 @@ export function CellRendererConditionParamOptions(props: PCellRendererConditionP
     ]
   ));
 
-  // The filters used for this condition
-  const [filterConditions, setFilterConditions] = useState<IFilter>();
-  const [previousFilterConditions, setPreviousFilterConditions] = useState<IFilter>();
-  // The attributes displayed in AttributeSelector
-  const [attributes, setAttributes] = useState<string[]>(Object.keys(filterConditions?.and_ || {}));
+  const updateFilterZone = (currentFilters: IFilter, originFilters: IFilter) => {
+    setFilterZone(defineZoneWithComponentList(
+      "dummy_object_type",
+      [
+        {
+          id: currentFilterComponentId,
+          filter: currentFilters,
+        },
+        {
+          id: originFilterComponentId,
+          filter: originFilters,
+        }
+      ]
+    ));
+  }
+  // Needed for the BottomButtons. Either adding the parameter or going back means that the filter
+  // making up the parameter should be cleared (it gets tied to the renderer somehow!)
+  const resetFilterZone = () => updateFilterZone(generateDefaultFilter(), generateDefaultFilter());
+
+  // The built-up filters for both tabs.
+  // The initial values are set in the useEffect below.
+  const [filtersForCurrent, setFiltersForCurrent] = useState<IFilter>();
+  const [filtersForOrigin, setFiltersForOrigin] = useState<IFilter>();
+
+  // The *previous* built-up filters for both tabs, used to know whether changes have been made.
+  // The initial values are set in the useEffect below.
+  const [previousFiltersForCurrent, setPreviousFiltersForCurrent] = useState<IFilter>();
+  const [previousFiltersForOrigin, setPreviousFiltersForOrigin] = useState<IFilter>();
+
+  // The attributes we're filtering on for both tabs.
+  // The initial values are set in the useEffect below.
+  const [attributesSelectedForCurrent, setAttributesSelectedForCurrent] = useState<string[]>();
+  const [attributesSelectedForOrigin, setAttributesSelectedForOrigin] = useState<string[]>();
+
   // Save a copy of the renderer before the filters are applied.
   // Because filter settings are changed by reference, if we go back to the first page, even without
   // setting any state, the changes will still apply. Then, the renderer is set to the value
   // stored here.
   const [rendererBeforeChanges, _setRendererBeforeChanges] = useState(deepCopy(renderer));
 
-  // Checks that the condition exists and has a value, stops the condition filters spreading across all conditions
+  // Runs upon opening the second page of the modal (to configure a new condition);
+  // Checks that the condition exists and has a value, and stops the condition filters spreading across all conditions.
+  // It also sets up states.
   useEffect(() => {
-    if (renderer?.props?.[paramName!] && paramName) {
-      const paramValue = renderer.props[paramName];
-      const filterValue = typeof paramValue === 'object' ? paramValue as IFilter : generateDefaultFilter();
-      setAttributes(Object.keys(filterValue.and_ || {}) || []);
-      setFilterConditions(filterValue);
-      setPreviousFilterConditions(previousRenderer?.props?.[paramName] as IFilter | undefined);
-      setFilterZone(defineZoneWithComponentList("dummy-object-for-remote-filters", [
-        { id: currentFilterComponentId, filter: filterValue },
-      ]));
-    } else {
-      setAttributes([]);
-      setFilterConditions(generateDefaultFilter());
-      setPreviousFilterConditions(generateDefaultFilter());
+    const conditionExists = renderer?.props?.[paramName!] && paramName;
+    if (conditionExists) {
+      // Extract the filter information that may already exist for this condition
+      const storedCondition = renderer!.props![paramName!];
+      const storedFiltersForCurrent = typeof storedCondition == "object"
+        ? (storedCondition as IDataPointConditionProp).filterForCurrent
+        : generateDefaultFilter();
+      const storedFiltersForOrigin = typeof storedCondition == "object"
+        ? (storedCondition as IDataPointConditionProp).filterForOrigin
+        : generateDefaultFilter();
+
+      // Set the states using this information
+      setFiltersForCurrent(storedFiltersForCurrent);
+      setFiltersForOrigin(storedFiltersForOrigin);
+      setAttributesSelectedForCurrent(Object.keys(storedFiltersForCurrent.and_ ?? {}) || []);
+      setAttributesSelectedForOrigin(Object.keys(storedFiltersForOrigin.and_ ?? {}) || []);
+      updateFilterZone(storedFiltersForCurrent, storedFiltersForOrigin);
+
+      // Do the same kind of thing for the previous conditions, except `undefined` is allowed
+      const previousCondition = previousRenderer?.props?.[paramName] as IDataPointConditionProp | undefined;
+      setPreviousFiltersForCurrent(previousCondition?.filterForCurrent);
+      setPreviousFiltersForOrigin(previousCondition?.filterForOrigin);
+    } else { // Set all the states to be blank
+      setFiltersForCurrent(generateDefaultFilter());
+      setFiltersForOrigin(generateDefaultFilter());
+
+      setPreviousFiltersForCurrent(generateDefaultFilter());
+      setPreviousFiltersForOrigin(generateDefaultFilter());
+
+      setAttributesSelectedForCurrent([]);
+      setAttributesSelectedForOrigin([]);
     }
   }, [paramName]);
 
+  // The `filterZone` is what is updated when the filters changed,
+  // so when it changes, extract the filters into their own states
   useEffect(() => {
-    const newFilter = generateFilter(filterZone, currentFilterComponentId);
-    setFilterConditions(newFilter);
+    const newFiltersForCurrent = generateFilter(filterZone, currentFilterComponentId);
+    const newFiltersForOrigin = generateFilter(filterZone, originFilterComponentId);
+
+    // Set the states
+    setFiltersForCurrent(newFiltersForCurrent);
+    setFiltersForOrigin(newFiltersForOrigin);
+
     setHasPendingChanges(() => {
       if (!renderer || !paramName) return false;
-      renderer!.props![paramName!] = newFilter ?? {};
+
+      // Apply the new filters to the renderer
+      const newCondition = {
+        filterForCurrent: newFiltersForCurrent ?? {},
+        filterForOrigin: newFiltersForOrigin ?? {},
+      } as IDataPointConditionProp;
+      renderer!.props![paramName!] = newCondition;
       setRenderer({ ...renderer });
-      return JSON.stringify(previousFilterConditions) !== JSON.stringify(newFilter);
+
+      return (
+        JSON.stringify(previousFiltersForCurrent) !== JSON.stringify(newFiltersForCurrent)
+        || JSON.stringify(previousFiltersForOrigin) !== JSON.stringify(newFiltersForOrigin)
+      );
     });
   }, [filterZone]);
-
-  // Needed for the BottomButtons. Either adding the parameter or going back means that the filter
-  // making up the parameter should be cleared (it gets tied to the renderer somehow!)
-  const resetFilterZone = () => setFilterZone(defineZoneWithComponentList("dummy-object-for-remote-filters", [
-    { id: currentFilterComponentId, filter: generateDefaultFilter() },
-  ]));
 
   const handleBack = () => {
     resetFilterZone();
@@ -155,12 +215,20 @@ export function CellRendererConditionParamOptions(props: PCellRendererConditionP
   };
 
   const handleSave = () => {
-    // delete empty params if no condition present
-    if (isEmptyObject(filterConditions?.and_ || {})) {
+    // Save the changes to the condition filters.
+    // If there's nothing in either filter, delete the param in the renderer to show that there
+    // is no condition (this will change the button on the previous page from Edit Condition to Add Condition).
+    // TODO Are the states storing the filters actually necessary? They're only ever modified via the zone
+    if (isEmptyObject(filtersForCurrent?.and_ || {}) && isEmptyObject(filtersForOrigin?.and_ || {})) {
       delete renderer!.props![paramName!];
     } else {
-      renderer!.props![paramName!] = filterConditions ?? {};
+      // TODO Reusable function for this?
+      renderer!.props![paramName!] = {
+        filterForCurrent: filtersForCurrent ?? {},
+        filterForOrigin: filtersForOrigin ?? {},
+      } as IDataPointConditionProp;
     }
+
     setRenderer({ ...renderer! });
     setHasPendingChanges(false);
     resetFilterZone();
@@ -208,8 +276,8 @@ export function CellRendererConditionParamOptions(props: PCellRendererConditionP
             displaySource
             recommendedFilterAvailable
             renderSearchBySource
-            attribute={attributes}
-            setAttributes={setAttributes}
+            attribute={attributesSelectedForCurrent ?? []}
+            setAttributes={setAttributesSelectedForCurrent}
             populatedFieldType="filter"
             onClean={() => {
               // We know that the component exists because it is set by default (filterZone state)
@@ -228,7 +296,7 @@ export function CellRendererConditionParamOptions(props: PCellRendererConditionP
             zone={filterZone}
             setZone={setFilterZone}
             componentId={currentFilterComponentId}
-            attributes={attributes}
+            attributes={attributesSelectedForCurrent ?? []}
           />
         </Tabs.Tab>
         <Tabs.Tab eventKey="origin" title="Origin">
