9820293fd4c1edb2195a644819017f6577bd15b4
Andrew Varley <av10@sanger.ac.uk>
2026-09-14 14:48:40 +0100

    Added minimal styling

--- components-ui/app/src/pages/Sandbox.tsx
+++ components-ui/app/src/pages/Sandbox.tsx
@@ -4,37 +4,44 @@ SPDX-FileCopyrightText: 2025 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-import { IconLinkTextsViewer } from "../tol-ui/src/general";
+import { IconLinkTextsViewer, VideoPlayer } from "../tol-ui/src/general";
 
 export function Sandbox() {
   return (
-    <IconLinkTextsViewer
-      data={[
-        {
-          link: "https://github.com/",
-          icon: "github",
-          config: "brands",
-          text: "GitHub",
-        },
-        {
-          link: "https://gitlab.com/",
-          icon: "gitlab",
-          config: "brands",
-          text: "GitLab",
-        },
-        {
-          link: "https://bluesky.app/",
-          icon: "bluesky",
-          config: "brands",
-          text: "Bluesky",
-        },
-        {
-          link: "https://www.linkedin.com/",
-          icon: "linkedin",
-          config: "brands",
-          text: "LinkedIn",
-        },
-      ]}
-    />
+    <>
+      <IconLinkTextsViewer
+        data={[
+          {
+            link: "https://github.com/",
+          },
+          {
+            link: "https://gitlab.com/",
+          },
+          {
+            link: "https://bluesky.app/",
+          },
+          {
+            link: "https://www.linkedin.com/",
+          },
+        ]}
+      />
+      <VideoPlayer
+        host="youtube"
+        videoId="M7lc1UVf-VE"
+        width={280}
+        height={157}
+      />
+      <h2>New Section</h2>
+      <IconLinkTextsViewer
+        data={[
+          {
+            link: "https://google.com/",
+            text: "Google",
+            icon: "download",
+          },
+        ]}
+      />
+
+    </>
   );
 }
