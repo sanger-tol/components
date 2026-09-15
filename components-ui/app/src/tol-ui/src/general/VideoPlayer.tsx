e13481a1e9903ff68e5cf2f903dc4fdb8fedc87d
Andrew Varley <av10@sanger.ac.uk>
2026-09-15 13:21:54 +0100

    More icon/video tweaks

--- components-ui/app/src/tol-ui/src/general/VideoPlayer.tsx
+++ components-ui/app/src/tol-ui/src/general/VideoPlayer.tsx
@@ -15,6 +15,27 @@ export interface PVideoPlayer {
   style?: React.CSSProperties;
 }
 
+export interface IVideoDetails {
+  host: "youtube" | "vimeo";
+  videoId: string;
+}
+
+export function getVideoDetails(url: string): IVideoDetails | undefined {
+  const youtubeMatch = url.match(
+    /(?:youtube\.com\/(?:watch\?v=|embed\/|shorts\/)|youtu\.be\/)([^?&/]+)/i,
+  );
+  if (youtubeMatch) return { host: "youtube", videoId: youtubeMatch[1] };
+
+  const vimeoMatch = url.match(/vimeo\.com\/(?:video\/)?(\d+)/i);
+  if (vimeoMatch) return { host: "vimeo", videoId: vimeoMatch[1] };
+
+  return undefined;
+}
+
+export function isVideo(url: string): boolean {
+  return getVideoDetails(url) !== undefined;
+}
+
 export function VideoPlayer(props: PVideoPlayer) {
   let { host = "youtube", videoId, width, height, style } = props;
 
